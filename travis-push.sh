d397463b30f6f4841be807ef9dc64c6e865d3978
Dorai Sitaram <ds26gte@yahoo.com>
2019-02-18 13:37:28 -0500

    Allow travis CI to push generated files to repo

---
+++ travis-push.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+git config --global user.email ds26gte@gmail.com
+git config --global user.name "Dorai Sitaram"
+
+cd docs
+git commit -m "committed at $(date)" adocex.html
+cd ..
+
+git remote rm origin
+git remote add origin https://${GITHUB_TOKEN}@github.com/ds26gte/curr-reorg >/dev/null 2>&1
+git push origin master --quiet
