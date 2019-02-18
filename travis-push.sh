dd1c09077183f771e7d5a28fdc0ad66934b6cb26
Dorai Sitaram <ds26gte@yahoo.com>
2019-02-18 13:57:35 -0500

    ...

--- travis-push.sh
+++ travis-push.sh
@@ -8,5 +8,7 @@ git commit -m "committed at $(date)" adocex.html
 cd ..
 
 git remote rm origin
-git remote add origin https://${GITHUB_TOKEN}@github.com/ds26gte/curr-reorg >/dev/null 2>&1
-git push origin master --quiet
+#git remote add origin https://${GITHUB_TOKEN}@github.com/ds26gte/curr-reorg >/dev/null 2>&1
+#git push origin master --quiet
+git remote add origin https://${GITHUB_TOKEN}@github.com/ds26gte/curr-reorg
+git push origin master
