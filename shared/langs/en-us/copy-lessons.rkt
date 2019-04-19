40240d0328799a9baf08eb59950d754d655c9c66
Dorai Sitaram <ds26gte@yahoo.com>
2019-04-18 20:37:17 -0400

    copy-lessons.rkt: Allow skipping over mangled lesson directories with a warning

--- shared/langs/en-us/copy-lessons.rkt
+++ shared/langs/en-us/copy-lessons.rkt
@@ -16,9 +16,13 @@
     (fprintf o "(~n")
     (for ((lesson *workbook-index*))
       (system (format "cp -pr $TOPDIR/lessons/~a/langs/en-us lessons/~a" lesson lesson))
-      (let ((lesson-index
-              (call-with-input-file
-                (format "lessons/~a/workbook-pages/lesson-index.rkt" lesson) read)))
+      (let* ((lesson-index-file (format "lessons/~a/workbook-pages/lesson-index.rkt" lesson))
+             (lesson-index
+               (cond ((file-exists? lesson-index-file)
+                      (call-with-input-file lesson-index-file read))
+                     (else
+                       (printf "WARNING: Lesson ~a is incorrectly organized~n" lesson)
+                       '()))))
         (for ((page lesson-index))
           (fprintf o "(~s ~s)~n" lesson page))))
     (fprintf o ")~n")))
