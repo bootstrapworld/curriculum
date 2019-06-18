9bda9efae8a54ec4da291f6d811bb36681e161dd
Ed Campos <34355200+edcamposjr22@users.noreply.github.com>
2019-06-18 07:52:42 -0700

    Update lesson-index.rkt

--- lessons/DefiningFunctions3/langs/en-us/workbook-pages/lesson-index.rkt
+++ lessons/DefiningFunctions3/langs/en-us/workbook-pages/lesson-index.rkt
@@ -6,11 +6,11 @@
 ("MappingExamplesWithCoE3.pdf" "landscape")
 "DesignRecipe1.pdf"
 ("PurposeStatement3ReadsStrongerClearer.pdf" "landscape")
-"MappingExamplesWithCoE3.pdf"
+"MappingExamplesWithCoE.pdf"
 "DesignRecipe1.pdf"
 ("PurposeStatement3ReadsStrongerClearer.pdf" "landscape")
-"MappingExamplesWithCoE3.pdf"
+"MappingExamplesWithCoE.pdf"
 "DesignRecipe1.pdf"
 ("PurposeStatement3ReadsStrongerClearer.pdf" "landscape")
-"MappingExamplesWithCoE3.pdf"
+"MappingExamplesWithCoE.pdf"
 )
