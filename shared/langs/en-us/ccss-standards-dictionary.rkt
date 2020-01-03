4cafa1946f2a05c2ef3059216084f6773e382a8b
Jennifer Poole <jen@bootstrapworld.org>
2020-01-03 14:46:23 -0600

    Add OK and CC ELA standards

--- shared/langs/en-us/ccss-standards-dictionary.rkt
+++ shared/langs/en-us/ccss-standards-dictionary.rkt
@@ -1314,5 +1314,272 @@
  ("HSS.MD.B.7"
   "Analyze decisions and strategies using probability concepts."
  )
+ ("WHST.6-8.1"
+  "Write arguments focused on discipline-specific content."
+ )
+ ("WHST.6-8.1.A"
+  "Introduce claim(s) about a topic or issue, acknowledge and distinguish the claim(s) from alternate or opposing claims, and organize the reasons and evidence logically."
+ )
+ ("WHST.6-8.1.B"
+  "Support claim(s) with logical reasoning and relevant, accurate data and evidence that demonstrate an understanding of the topic or text, using credible sources."
+ )
+ ("WHST.6-8.1.C"
+  "Use words, phrases, and clauses to create cohesion and clarify the relationships among claim(s), counterclaims, reasons, and evidence."
+ )
+ ("WHST.6-8.1.D"
+  "Establish and maintain a formal style."
+ )
+ ("WHST.6-8.1.E"
+  "Provide a concluding statement or section that follows from and supports the argument presented."
+ )
+ ("WHST.6-8.2"
+  "Write informative/explanatory texts, including the narration of historical events, scientific procedures/ experiments, or technical processes."
+ )
+ ("WHST.6-8.2.A"
+  "Introduce a topic clearly, previewing what is to follow; organize ideas, concepts, and information into broader categories as appropriate to achieving purpose; include formatting (e.g., headings), graphics (e.g., charts, tables), and multimedia when useful to aiding comprehension."
+ )
+ ("WHST.6-8.2.B"
+  "Develop the topic with relevant, well-chosen facts, definitions, concrete details, quotations, or other information and examples."
+ )
+ ("WHST.6-8.2.C"
+  "Use appropriate and varied transitions to create cohesion and clarify the relationships among ideas and concepts."
+ )
+ ("WHST.6-8.2.D"
+  "Use precise language and domain-specific vocabulary to inform about or explain the topic."
+ )
+ ("WHST.6-8.2.E"
+  "Establish and maintain a formal style and objective tone."
+ )
+ ("WHST.6-8.2.F"
+  "Provide a concluding statement or section that follows from and supports the information or explanation presented."
+ )
+ ("WHST.6-8.4"
+  "Produce clear and coherent writing in which the development, organization, and style are appropriate to task, purpose, and audience."
+ )
+ ("WHST.6-8.5"
+  "With some guidance and support from peers and adults, develop and strengthen writing as needed by planning, revising, editing, rewriting, or trying a new approach, focusing on how well purpose and audience have been addressed."
+ )
+ ("WHST.6-8.6"
+  "Use technology, including the Internet, to produce and publish writing and present the relationships between information and ideas clearly and efficiently."
+ )
+ ("WHST.6-8.7"
+  "Conduct short research projects to answer a question (including a self-generated question), drawing on several sources and generating additional related, focused questions that allow for multiple avenues of exploration."
+ )
+ ("WHST.6-8.8"
+  "Gather relevant information from multiple print and digital sources, using search terms effectively; assess the credibility and accuracy of each source; and quote or paraphrase the data and conclusions of others while avoiding plagiarism and following a standard format for citation."
+ )
+ ("WHST.6-8.9"
+  "Draw evidence from informational texts to support analysis, reflection, and research."
+ )
+ ("WHST.6-8.10"
+  "Write routinely over extended time frames (time for reflection and revision) and shorter time frames (a single sitting or a day or two) for a range of discipline-specific tasks, purposes, and audiences."
+ )
+ ("RST.6-8.1"
+  "Cite specific textual evidence to support analysis of science and technical texts."
+ )
+ ("RST.6-8.2"
+  "Determine the central ideas or conclusions of a text; provide an accurate summary of the text distinct from prior knowledge or opinions."
+ )
+ ("RST.6-8.3"
+  "Follow precisely a multistep procedure when carrying out experiments, taking measurements, or performing technical tasks."
+ )
+ ("RST.6-8.4"
+  "Determine the meaning of symbols, key terms, and other domain-specific words and phrases as they are used in a specific scientific or technical context relevant to grades 6-8 texts and topics."
+ )
+ ("RST.6-8.5"
+  "Analyze the structure an author uses to organize a text, including how the major sections contribute to the whole and to an understanding of the topic."
+ )
+ ("RST.6-8.6"
+  "Analyze the author's purpose in providing an explanation, describing a procedure, or discussing an experiment in a text."
+ )
+ ("RST.6-8.7"
+  "Integrate quantitative or technical information expressed in words in a text with a version of that information expressed visually (e.g., in a flowchart, diagram, model, graph, or table)."
+ )
+ ("RST.6-8.8"
+  "Distinguish among facts, reasoned judgment based on research findings, and speculation in a text."
+ )
+ ("RST.6-8.9"
+  "Compare and contrast the information gained from experiments, simulations, video, or multimedia sources with that gained from reading a text on the same topic."
+ )
+ ("RST.6-8.10"
+  "By the end of grade 8, read and comprehend science/technical texts in the grades 6-8 text complexity band independently and proficiently."
+ )
+ ("WHST.9-10.1"
+  "Write arguments focused on discipline-specific content."
+ )
+ ("WHST.9-10.1.A"
+  "Introduce precise claim(s), distinguish the claim(s) from alternate or opposing claims, and create an organization that establishes clear relationships among the claim(s), counterclaims, reasons, and evidence."
+ )
+ ("WHST.9-10.1.B"
+  "Develop claim(s) and counterclaims fairly, supplying data and evidence for each while pointing out the strengths and limitations of both claim(s) and counterclaims in a discipline-appropriate form and in a manner that anticipates the audience's knowledge level and concerns."
+ )
+ ("WHST.9-10.1.C"
+  "Use words, phrases, and clauses to link the major sections of the text, create cohesion, and clarify the relationships between claim(s) and reasons, between reasons and evidence, and between claim(s) and counterclaims."
+ )
+ ("WHST.9-10.1.D"
+  "Establish and maintain a formal style and objective tone while attending to the norms and conventions of the discipline in which they are writing."
+ )
+ ("WHST.9-10.1.E"
+  "Provide a concluding statement or section that follows from or supports the argument presented."
+ )
+ ("WHST.9-10.2"
+  "Write informative/explanatory texts, including the narration of historical events, scientific procedures/ experiments, or technical processes."
+ )
+ ("WHST.9-10.2.A"
+  "Introduce a topic and organize ideas, concepts, and information to make important connections and distinctions; include formatting (e.g., headings), graphics (e.g., figures, tables), and multimedia when useful to aiding comprehension."
+ )
+ ("WHST.9-10.2.B"
+  "Develop the topic with well-chosen, relevant, and sufficient facts, extended definitions, concrete details, quotations, or other information and examples appropriate to the audience's knowledge of the topic."
+ )
+ ("WHST.9-10.2.C"
+  "Use varied transitions and sentence structures to link the major sections of the text, create cohesion, and clarify the relationships among ideas and concepts."
+ )
+ ("WHST.9-10.2.D"
+  "Use precise language and domain-specific vocabulary to manage the complexity of the topic and convey a style appropriate to the discipline and context as well as to the expertise of likely readers."
+ )
+ ("WHST.9-10.2.E"
+  "Establish and maintain a formal style and objective tone while attending to the norms and conventions of the discipline in which they are writing."
+ )
+ ("WHST.9-10.2.F"
+  "Provide a concluding statement or section that follows from and supports the information or explanation presented (e.g., articulating implications or the significance of the topic)."
+ )
+ ("WHST.9-10.4"
+  "Produce clear and coherent writing in which the development, organization, and style are appropriate to task, purpose, and audience."
+ )
+ ("WHST.9-10.5"
+  "Develop and strengthen writing as needed by planning, revising, editing, rewriting, or trying a new approach, focusing on addressing what is most significant for a specific purpose and audience."
+ )
+ ("WHST.9-10.6"
+  "Use technology, including the Internet, to produce, publish, and update individual or shared writing products, taking advantage of technology's capacity to link to other information and to display information flexibly and dynamically."
+ )
+ ("WHST.9-10.7"
+  "Conduct short as well as more sustained research projects to answer a question (including a self-generated question) or solve a problem; narrow or broaden the inquiry when appropriate; synthesize multiple sources on the subject, demonstrating understanding of the subject under investigation."
+ )
+ ("WHST.9-10.8"
+  "Gather relevant information from multiple authoritative print and digital sources, using advanced searches effectively; assess the usefulness of each source in answering the research question; integrate information into the text selectively to maintain the flow of ideas, avoiding plagiarism and following a standard format for citation."
+ )
+ ("WHST.9-10.9"
+  "Draw evidence from informational texts to support analysis, reflection, and research."
+ )
+ ("WHST.9-10.10"
+  "Write routinely over extended time frames (time for reflection and revision) and shorter time frames (a single sitting or a day or two) for a range of discipline-specific tasks, purposes, and audiences."
+ )
+ ("RST.9-10.1"
+  "Cite specific textual evidence to support analysis of science and technical texts, attending to the precise details of explanations or descriptions."
+ )
+ ("RST.9-10.2"
+  "Determine the central ideas or conclusions of a text; trace the text's explanation or depiction of a complex process, phenomenon, or concept; provide an accurate summary of the text."
+ )
+ ("RST.9-10.3"
+  "Follow precisely a complex multistep procedure when carrying out experiments, taking measurements, or performing technical tasks, attending to special cases or exceptions defined in the text."
+ )
+ ("RST.9-10.4"
+  "Determine the meaning of symbols, key terms, and other domain-specific words and phrases as they are used in a specific scientific or technical context relevant to grades 9-10 texts and topics."
+ )
+ ("RST.9-10.5"
+  "Analyze the structure of the relationships among concepts in a text, including relationships among key terms (e.g., force, friction, reaction force, energy)."
+ )
+ ("RST.9-10.6"
+  "Analyze the author's purpose in providing an explanation, describing a procedure, or discussing an experiment in a text, defining the question the author seeks to address."
+ )
+ ("RST.9-10.7"
+  "Translate quantitative or technical information expressed in words in a text into visual form (e.g., a table or chart) and translate information expressed visually or mathematically (e.g., in an equation) into words."
+ )
+ ("RST.9-10.8"
+  "Assess the extent to which the reasoning and evidence in a text support the author's claim or a recommendation for solving a scientific or technical problem."
+ )
+ ("RST.9-10.9"
+  "Compare and contrast findings presented in a text to those from other sources (including their own experiments), noting when the findings support or contradict previous explanations or accounts."
+ )
+ ("RST.9-10.10"
+  "By the end of grade 10, read and comprehend science/technical texts in the grades 9-10 text complexity band independently and proficiently."
+ )
+ ("WHST.11-12.1"
+  "Write arguments focused on discipline-specific content."
+ )
+ ("WHST.11-12.1.A"
+  "Introduce precise, knowledgeable claim(s), establish the significance of the claim(s), distinguish the claim(s) from alternate or opposing claims, and create an organization that logically sequences the claim(s), counterclaims, reasons, and evidence."
+ )
+ ("WHST.11-12.1.B"
+  "Develop claim(s) and counterclaims fairly and thoroughly, supplying the most relevant data and evidence for each while pointing out the strengths and limitations of both claim(s) and counterclaims in a discipline-appropriate form that anticipates the audience's knowledge level, concerns, values, and possible biases."
+ )
+ ("WHST.11-12.1.C"
+  "Use words, phrases, and clauses as well as varied syntax to link the major sections of the text, create cohesion, and clarify the relationships between claim(s) and reasons, between reasons and evidence, and between claim(s) and counterclaims."
+ )
+ ("WHST.11-12.1.D"
+  "Establish and maintain a formal style and objective tone while attending to the norms and conventions of the discipline in which they are writing."
+ )
+ ("WHST.11-12.1.E"
+  "Provide a concluding statement or section that follows from or supports the argument presented."
+ )
+ ("WHST.11-12.2"
+  "Write informative/explanatory texts, including the narration of historical events, scientific procedures/experiments, or technical processes."
+ )
+ ("WHST.11-12.2.A"
+  "Introduce a topic and organize complex ideas, concepts, and information so that each new element builds on that which precedes it to create a unified whole; include formatting (e.g., headings), graphics (e.g., figures, tables), and multimedia when useful to aiding comprehension."
+ )
+ ("WHST.11-12.2.B"
+  "Develop the topic thoroughly by selecting the most significant and relevant facts, extended definitions, concrete details, quotations, or other information and examples appropriate to the audience's knowledge of the topic."
+ )
+ ("WHST.11-12.2.C"
+  "Use varied transitions and sentence structures to link the major sections of the text, create cohesion, and clarify the relationships among complex ideas and concepts."
+ )
+ ("WHST.11-12.2.D"
+  "Use precise language, domain-specific vocabulary and techniques such as metaphor, simile, and analogy to manage the complexity of the topic; convey a knowledgeable stance in a style that responds to the discipline and context as well as to the expertise of likely readers."
+ )
+ ("WHST.11-12.2.E"
+  "Provide a concluding statement or section that follows from and supports the information or explanation provided (e.g., articulating implications or the significance of the topic)."
+ )
+ ("WHST.11-12.4"
+  "Produce clear and coherent writing in which the development, organization, and style are appropriate to task, purpose, and audience."
+ )
+ ("WHST.11-12.5"
+  "Develop and strengthen writing as needed by planning, revising, editing, rewriting, or trying a new approach, focusing on addressing what is most significant for a specific purpose and audience."
+ )
+ ("WHST.11-12.6"
+  "Use technology, including the Internet, to produce, publish, and update individual or shared writing products in response to ongoing feedback, including new arguments or information."
+ )
+ ("WHST.11-12.7"
+  "Conduct short as well as more sustained research projects to answer a question (including a self-generated question) or solve a problem; narrow or broaden the inquiry when appropriate; synthesize multiple sources on the subject, demonstrating understanding of the subject under investigation."
+ )
+ ("WHST.11-12.8"
+  "Gather relevant information from multiple authoritative print and digital sources, using advanced searches effectively; assess the strengths and limitations of each source in terms of the specific task, purpose, and audience; integrate information into the text selectively to maintain the flow of ideas, avoiding plagiarism and overreliance on any one source and following a standard format for citation."
+ )
+ ("WHST.11-12.9"
+  "Draw evidence from informational texts to support analysis, reflection, and research."
+ )
+ ("WHST.11-12.10"
+  "Write routinely over extended time frames (time for reflection and revision) and shorter time frames (a single sitting or a day or two) for a range of discipline-specific tasks, purposes, and audiences."
+ )
+ ("RST.11-12.1"
+  "Cite specific textual evidence to support analysis of science and technical texts, attending to important distinctions the author makes and to any gaps or inconsistencies in the account."
+ )
+ ("RST.11-12.2"
+  "Determine the central ideas or conclusions of a text; summarize complex concepts, processes, or information presented in a text by paraphrasing them in simpler but still accurate terms."
+ )
+ ("RST.11-12.3"
+  "Follow precisely a complex multistep procedure when carrying out experiments, taking measurements, or performing technical tasks; analyze the specific results based on explanations in the text."
+ )
+ ("RST.11-12.4"
+  "Determine the meaning of symbols, key terms, and other domain-specific words and phrases as they are used in a specific scientific or technical context relevant to grades 11-12 texts and topics."
+ )
+ ("RST.11-12.5"
+  "Analyze how the text structures information or ideas into categories or hierarchies, demonstrating understanding of the information or ideas."
+ )
+ ("RST.11-12.6"
+  "Analyze the author's purpose in providing an explanation, describing a procedure, or discussing an experiment in a text, identifying important issues that remain unresolved."
+ )
+ ("RST.11-12.7"
+  "Integrate and evaluate multiple sources of information presented in diverse formats and media (e.g., quantitative data, video, multimedia) in order to address a question or solve a problem."
+ )
+ ("RST.11-12.8"
+  "Evaluate the hypotheses, data, analysis, and conclusions in a science or technical text, verifying the data when possible and corroborating or challenging conclusions with other sources of information."
+ )
+ ("RST.11-12.9"
+  "Synthesize information from a range of sources (e.g., texts, experiments, simulations) into a coherent understanding of a process, phenomenon, or concept, resolving conflicting information when possible."
+ )
+ ("RST.11-12.10"
+  "By the end of grade 12, read and comprehend science/technical texts in the grades 11-CCR text complexity band independently and proficiently."
+ )
  ))
  
