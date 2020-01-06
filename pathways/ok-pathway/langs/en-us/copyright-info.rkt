a7f776fa7b45f29bd3eac775242bb665ce01ce16
Jennifer Poole <jen@bootstrapworld.org>
2020-01-06 13:38:27 -0600

    One lesson and pathway for debugging

---
+++ pathways/ok-pathway/langs/en-us/copyright-info.rkt
@@ -0,0 +1,4 @@
+(
+ ("name" "Bootstrap:Integrated Oklahoma")
+ ("author" "Jen Poole")
+ )
---
+++ pathways/ok-pathway/langs/en-us/workbook-index.rkt
@@ -0,0 +1,9 @@
+(
+ "IntroToIDE"
+; "IntroToCoE"
+; "IntroToContracts"
+; "FunctionComposition1"
+; "DefiningValues1"
+; "flags-of-the-world"
+; "FunctionApplications2"
+)
