55aa136962200cc3cb1f106fba35d966c5d8a956
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-03-09 16:57:23 -0400

    [DS] TONS of fixes to workbook

--- lessons/ds-checking-your-work/langs/en-us/workbook-pages/workbook-pages.rkt
+++ lessons/ds-checking-your-work/langs/en-us/workbook-pages/workbook-pages.rkt
@@ -1,7 +1,5 @@
 (
  "checking-your-work.adoc"
- "chaining-methods.adoc"
- "chaining-methods-order-matters.adoc"
  "trust-but-verify.adoc"
  "trust-but-verify-2.adoc"
  "whats-on-your-mind.adoc"
--- lessons/ds-method-chaining/langs/en-us/workbook-pages/workbook-pages.rkt
+++ lessons/ds-method-chaining/langs/en-us/workbook-pages/workbook-pages.rkt
@@ -2,7 +2,5 @@
  "method-chaining.adoc"
  "chaining-methods.adoc"
  "chaining-methods-order-matters.adoc"
- "trust-but-verify.adoc"
- "trust-but-verify-2.adoc"
  "whats-on-your-mind.adoc"
  )
