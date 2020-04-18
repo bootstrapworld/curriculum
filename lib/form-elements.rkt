93d115689ea2f3c19becb5babab24f427d3bad18
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-04-18 18:55:22 -0400

    actually make gdrive imports have styles - WOOHOO

--- lib/form-elements.rkt
+++ lib/form-elements.rkt
@@ -4,7 +4,7 @@
 
 (define *macro-list*
   '(
-    ("lesson-intro-table" ".left-header,cols=\"20a,80a\",stripes=none")
+    ("lesson-intro-table" ".left-header.preamble,cols=\"20a,80a\",stripes=none")
     ("lesson-table" ".lesson-table,cols=\"10a,4a,18a,14a\",options=\"header\"")
     ("standards-table" ".standards-table,cols=\"50a,50a\",options=\"header\"")
     ))
