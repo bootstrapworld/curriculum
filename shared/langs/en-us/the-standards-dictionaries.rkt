d4e4573a5860d2b3b1cb704b9d6c6af1635b5539
Dorai Sitaram <ds26gte@gmail.com>
2020-05-30 08:28:21 -0400

    Include the NGSS standards dictionary in the global standards list!

--- shared/langs/en-us/the-standards-dictionaries.rkt
+++ shared/langs/en-us/the-standards-dictionaries.rkt
@@ -7,6 +7,7 @@
 
 (require "ccss-standards-dictionary.rkt")
 (require "csta-standards-dictionary.rkt")
+(require "ngss-standards-dictionary.rkt")
 (require "ok-standards-dictionary.rkt")
 (require "old-standards-dictionary.rkt")
 
@@ -14,6 +15,7 @@
   (list
     (list "CCSS" "Common Core State" *ccss-standards-list*)
     (list "CSTA" #f *csta-standards-list*)
+    (list "NGSS" "Next-Gen Science" *ngss-standards-list*)
     (list "OK" "Oklahoma" *ok-standards-list*)
     (list "OLD" "Older" *old-standards-list*)
     ))
