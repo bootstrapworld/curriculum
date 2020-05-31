54398d77f3caa135539d42a79a1b3f3f5ecd6379
Dorai Sitaram <ds26gte@gmail.com>
2020-05-31 11:40:46 -0400

    HOC lessons should have langs/en-us subdirs. Lesson dirs are allowed to lack adoc files if index.{s,}html present. Lesson dirs are allowed to lack pages/ dir. CC-ELA loading typo fixed.

--- shared/langs/en-us/the-standards-dictionaries.rkt
+++ shared/langs/en-us/the-standards-dictionaries.rkt
@@ -5,17 +5,17 @@
   expand-dict-abbrev
   )
 
-(require "cc-math-standards-dictionary.rkt")
 (require "cc-ela-standards-dictionary.rkt")
-(require "ngss-standards-dictionary.rkt")
-(require "ok-standards-dictionary.rkt")
+(require "cc-math-standards-dictionary.rkt")
 (require "csta-standards-dictionary.rkt")
 (require "k12cs-standards-dictionary.rkt")
+(require "ngss-standards-dictionary.rkt")
+(require "ok-standards-dictionary.rkt")
 
 (define *standards-list*
   (list
+    (list "CC-ELA" "Common Core ELA" *cc-ela-standards-list*)
     (list "CC-Math" "Common Core Math" *cc-math-standards-list*)
-    (list "CC-ELA" "Common Core ELA" *cc-math-standards-list*)
     (list "CSTA" #f *csta-standards-list*)
     (list "K12CS" "K-12CS" *k12cs-standards-list*)
     (list "NGSS" "Next-Gen Science" *ngss-standards-list*)
