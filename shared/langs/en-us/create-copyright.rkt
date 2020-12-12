fec745e7db1ec0fa1ee7737f2b43156a9152c293
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2020-12-11 20:17:17 -0500

    [lib] strengthen the language around our copyright NOT covering trainings

--- shared/langs/en-us/create-copyright.rkt
+++ shared/langs/en-us/create-copyright.rkt
@@ -5,9 +5,7 @@
 (define (create-copyright name author)
   (format "~a by ~a
 is licensed under a
-https://creativecommons.org/licenses/by-nc-nd/4.0/[Creative Commons 4.0 Unported License].
-Based on a work at https://www.bootstrapworld.org[www.BootstrapWorld.org].
-Permissions beyond the scope of this license may be available by contacting
-mailto:schanzer@BootstrapWorld.org[schanzer@BootstrapWorld.org]. "
+https://creativecommons.org/licenses/by-nc-nd/4.0/[Creative Commons 4.0 Unported License]. This license does not grant permission to run training or professional development. Offering training or professional development with materials substantially derived from Bootstrap must be approved in writing by a Bootstrap Director. Permissions beyond the scope of this license, such as to run training, may be available by contacting
+mailto:contact@BootstrapWorld.org[contact@BootstrapWorld.org]. "
           name author
           ))
