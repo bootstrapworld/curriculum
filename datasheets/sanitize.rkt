375ddeffd2ccb90510efb7ee5e1f6a86dfddc08a
Dorai Sitaram <ds26gte@gmail.com>
2021-01-05 10:28:52 -0500

    1. In the admin file, [.answer] annotates paragraphs that serve as stock    answers to .common questions. 2. sanitize.rkt now inserts the stock answer when it finds :common:

--- datasheets/sanitize.rkt
+++ datasheets/sanitize.rkt
@@ -30,9 +30,9 @@
                       (when (regexp-match
                               "^// +(Required Question|Common Question|Question|_) +:.+?: *$"
                               ln)
-                        (let ((tag 
-                                (regexp-replace 
-                                  "^// +(Required Question|Common Question|Question|_) +:(.+?): *$" 
+                        (let ((tag
+                                (regexp-replace
+                                  "^// +(Required Question|Common Question|Question|_) +:(.+?): *$"
                                   ln "\\2")))
                           (set! tags (cons tag tags))))))
               (loop))))))
@@ -43,28 +43,31 @@
         (student-tags (collect-tags)))
     ;(printf "a-tags = ~a\n" admin-tags)
     ;(printf "s-tags = ~a\n" student-tags)
-    (if (equal? admin-tags student-tags)
-        (printf "Student file is OK\n")
-        (let ((num-admin-tags (length admin-tags))
-              (num-student-tags (length student-tags)))
-          (cond ((< num-admin-tags num-student-tags)
-                 (printf "Student file has extra tags\n"))
-                ((> num-admin-tags num-student-tags)
-                 (printf "Student file missing tags\n")))
-        (let loop ((i 0) (j 0))
-          (unless (or (>= i num-admin-tags)
-                      (>= j num-student-tags))
-            (let ((a-tag (list-ref admin-tags i))
-                  (s-tag (list-ref student-tags j)))
-              (if (not (equal? a-tag s-tag))
-                  (cond ((not (member s-tag admin-tags))
-                         (printf "Student file has unrecognized tag ~a\n" s-tag)
-                         (loop i (+ j 1)))
-                        (else
-                          (printf "Student file has tag ~a where admin file has ~a\n"
-                                  s-tag a-tag)
-                          (loop (+ i 1) (+ j 1))))
-                  (loop (+ i 1) (+ j 1))))))))))
+    (cond ((equal? admin-tags student-tags)
+           (printf "Student file has all the tags in the right order\n")
+           #t)
+          (else
+            (let ((num-admin-tags (length admin-tags))
+                  (num-student-tags (length student-tags)))
+              (cond ((< num-admin-tags num-student-tags)
+                     (printf "Student file has extra tags\n"))
+                    ((> num-admin-tags num-student-tags)
+                     (printf "Student file missing tags\n")))
+              (let loop ((i 0) (j 0))
+                (unless (or (>= i num-admin-tags)
+                            (>= j num-student-tags))
+                  (let ((a-tag (list-ref admin-tags i))
+                        (s-tag (list-ref student-tags j)))
+                    (if (not (equal? a-tag s-tag))
+                        (cond ((not (member s-tag admin-tags))
+                               (printf "Student file has unrecognized tag ~a\n" s-tag)
+                               (loop i (+ j 1)))
+                              (else
+                                (printf "Student file has tag ~a where admin file has ~a\n"
+                                        s-tag a-tag)
+                                (loop (+ i 1) (+ j 1))))
+                        (loop (+ i 1) (+ j 1)))))))
+            #f))))
 
 (define (check-questions-answered)
   (call-with-input-file *student-file*
@@ -73,26 +76,69 @@
         (let loop ()
           (let ((ln (read-line i)))
             (unless (eof-object? ln)
+              ;(printf "XXX ln = ~s\n" ln)
               (when (regexp-match "^// +(Common|Required) Question +:.+:?: *$" ln)
-                (let ((tag (regexp-replace "^// +(Common|Required) Question +:(.+?): *$" ln "\\1"))
+                ;(printf "XXX found a common/reqd q ~s\n" ln)
+                (let ((tag (regexp-replace "^// +(Common|Required) Question +(:.+?:) *$" ln "\\2"))
                       (answered? #f))
                   (let loop2 ()
                     (let ((ln (read-line i)))
+                      ;(printf "XXX loop2 ln = ~s\n" ln)
                       (unless (or (eof-object? ln)
                                   (regexp-match "^include::" ln))
                         (cond ((regexp-match "^//" ln) #f)
                               ((regexp-match "^ *$" ln) #f)
-                              (else (set! answered? #t)))))
-                    (loop2))
+                              (else (set! answered? #t)))
+                        (unless answered? (loop2)))))
                   (unless answered?
                     (set! ok? #f)
-                    (printf "Student file has unanswered question ~a\n")))))))
+                    (printf "Student file has unanswered question ~a\n" tag))))
+              (loop))))
         (when ok?
-          (printf "All required/common questions answered\n"))))))
+          (printf "All required/common questions answered\n"))
+        ok?))))
+
+(define (common-answer-include? ln)
+  (let ((x (regexp-match "^// +common-answer::(.*)" ln)))
+    (and x
+         (string-append "include::" (list-ref x 1)))))
+
+(define (insert-common? ln)
+  (regexp-match "^ *:common: *$" ln))
+
+(define (check-common-answers)
+  (let ((temp-file (regexp-replace "([^ ]+)-student.adoc" *student-file*
+                                   "\\1-student-temp.adoc"))
+        (common-answer-inserted? #f)
+        (common-answer-fragment #f))
+    (call-with-input-file *student-file*
+      (lambda (i)
+        (call-with-output-file temp-file
+          (lambda (o)
+            (let loop ()
+              (let ((ln (read-line i)))
+                (unless (eof-object? ln)
+                  (cond ((common-answer-include? ln)
+                         => (lambda (x) (set! common-answer-fragment x)))
+                        ((insert-common? ln)
+                         (cond (common-answer-fragment
+                                 (set! common-answer-inserted? #t)
+                                 (fprintf o "\n~a\n\n" common-answer-fragment))
+                               (else
+                                 (printf "WARNING: :common: used incorrectly\n"))))
+                        (else (display ln o) (newline o)))
+                  (loop)))))
+          #:exists 'replace)))
+    (when common-answer-inserted?
+      (printf "Stock answers needed to be inserted\n")
+      (rename-file-or-directory temp-file *student-file* #t))))
 
 (define (sanitize)
-  (check-tag-integrity)
-  (check-questions-answered))
+  (and
+    (check-tag-integrity)
+    (check-questions-answered)
+    (check-common-answers))
+  (void))
 
 (sanitize)
 
