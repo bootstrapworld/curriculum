cfc9b04423297276fd0cfd93905333b8e00c759c
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2021-02-10 14:12:31 -0500

    [lib

---
+++ shared/langs/en-us/textbook-connected-math.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(provide *cmp-standards-list*)
+
+(define *cmp-standards-list*
+'(
+
+  ; We are borrowing the infrastructure used for standards alignment,
+  ; but in this case we're really talking about *chapters*
+
+  ("Chapter 1" "Functions")
+  ("Chapter 2" "Linear Relationships")
+  ("Chapter 3" "Simplifying and Solving")
+  ("Chapter 4" "Systems and Equations")
+  ("Chapter 5" "Sequences")
+  ("Chapter 6" "Modeling Two-Variable Data")
+  ("Chapter 7" "Exponential Functions")
+  ("Chapter 8" "Quadratic Functions")
+  ("Chapter 9" "Solving Quadratic and Inequalities")
+  ("Chapter 10" "Solving Complex Equations")
+  ("Chapter 11" "Functions and Data")
+ ))
+
