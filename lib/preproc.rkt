0b25ed222bdcd3908ad5237a6d5cdaf076eaad4a
Dorai Sitaram <ds26gte@gmail.com>
2021-06-08 23:07:51 -0400

    sexp->arith (used by sexp->pyret): match sexp->block (used by sexp->wescheme) for styling of blanks and the various atomic datatypes.

--- lib/preproc.rkt
+++ lib/preproc.rkt
@@ -1565,8 +1565,7 @@
           [(< n 10) ".studentAnswerMedium"]
           [else ".studentAnswerLong"])))
 
-(define (answer-block-fill-length e)
-  "")
+(define answer-block-fill-length answer-fill-length)
 
 (define (infix-sexp->math a e #:wrap [wrap #t] #:encloser [encloser #f]
                           #:parens [parens #f] #:first [first #f])
@@ -1609,12 +1608,17 @@
 (define (sexp->arith e #:pyret [pyret #f] #:wrap [wrap #f]
                      #:encloser [encloser #f] #:parens [parens #f] #:first [first #f] #:tex [tex #f])
   ;(printf "doing sexp->arith ~s l:~s w:~s e:~s p:~s\n" e pyret wrap encloser parens)
-  (cond [(number? e) (format "~a" e)]
+  (cond [(member e '(true false)) (let ([x (format "~a" e)])
+                                    (if pyret (enclose-span ".value.wescheme-boolean" x) x))]
+        [(number? e) (let ([x (format "~a" e)])
+                       (if pyret (enclose-span ".value.wescheme-number" x) x))]
         [(and (symbol? e) pyret
               (memq e '(BSLeaveAHoleHere BSLeaveAHoleHere2 BSLeaveAHoleHere3)))
-         (enclose-span ".studentAnswer" (format "~a" e))]
-        [(symbol? e) (sym-to-adocstr e #:pyret pyret #:tex tex)]
-        [(string? e) (format "~s" e)]
+         (enclose-span ".studentAnswer" (format "~a" e))] ;CHECK
+        [(symbol? e) (let ([x (sym-to-adocstr e #:pyret pyret #:tex tex)])
+                       (if pyret (enclose-span ".value.wescheme-symbol" x) x))]
+        [(string? e) (let ([x (format "~s" e)])
+                       (if pyret (enclose-span ".value.wescheme-string" x) x))]
         [(answer? e) (let* ([e (cadr e)]
                             [fill-len (answer-fill-length e)])
                        ;(printf "answer frag found: ~s\n" e)
