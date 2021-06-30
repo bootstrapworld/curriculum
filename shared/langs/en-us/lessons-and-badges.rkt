98ffd3ae6b229973514587e71a89808976831224
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2021-06-30 14:29:37 -0400

    [Lib] Badges? We dont need to steenkin badges

---
+++ shared/langs/en-us/lessons-and-badges.rkt
@@ -0,0 +1,218 @@
+#lang racket
+
+;FIXME what wd be the canonical location of this file?
+;      the pathway top dir?
+
+(provide *lessons-and-standards*)
+
+(define *lessons-and-standards*
+  '(
+; core
+
+    ("contracts" 
+        )
+
+    ("defining-functions" 
+        "MP.4" ; STEMworks
+        )
+
+    ("defining-values" 
+        "MP.2"
+        )
+
+    ("simple-dataypes" 
+        ) 
+; math
+   
+    ("combinatorics-permutation"5"
+        )
+
+    ("combinatorics-combination"
+        )
+
+    ("coordinates" 
+        "MP.2"
+        )
+
+    ("distance-formula" 
+        "MP.2"
+        "MP.3" ; STEMworks
+        )
+
+    ("error-analysis" 
+        "MP.2"
+        )
+
+    ("flags" 
+
+        ) 
+
+    ("function-composition" 
+        "MP.1"
+        "MP.5"
+        )
+
+    ("functions-for-character-animation" 
+        "MP.2"
+        "MP.4" ; STEMworks
+        )
+
+    ("inequalities-collision"
+        "MP.3" ; STEMworks
+        )
+
+    ("inequalities1-simple" 
+        "MP.1"
+        "MP.4"
+        )
+
+    ("inequalities2-compound" 
+
+        )
+
+    ("inequalities3-sam"
+        "MP.4" ; STEMworks
+        )    
+
+    ("making-game-images" 
+        "MP.6"
+        )
+
+    ("numbers-inside-videogames" 
+        "MP.2"
+        )
+
+    ("order-of-operations" 
+
+        "MP.1"
+        "MP.2"
+        "MP.3" ; STEMworks
+        "MP.4" ; STEMworks
+        )
+
+    ("piecewise-functions" 
+        "MP.8"
+        )
+
+    ("player-animation" 
+        "MP.4"     ; STEMworks
+        )      
+
+    ("problem-decomposition"
+        "MP.1"
+        "MP.3"     ; STEMworks
+        "MP.4"     ; STEMworks
+        "MP.6"
+        )
+        
+    ("restating-the-problem" 
+        "MP.1"
+        "MP.7"
+        )
+
+    ("solving-word-problems" 
+        "MP.4" ; STEMworks
+        )
+
+    ("surface-area-rect-prism"
+        "MP.4" ; STEMworks
+        )
+
+    ;data science
+    
+    
+    ("checking-your-work" 
+        "MP.3" ; STEMworks
+        )
+
+    ("choosing-your-dataset" 
+
+        )
+
+    ("correlations" 
+        "MP.3" ; STEMworks
+        "MP.4" ; STEMworks
+        )
+    
+      ("defining-table-functions" 
+        )
+
+    ("displays-and-lookups" 
+
+        )
+
+    ("ds-intro" 
+        "MP.3" ; STEMworks
+        )
+
+    ("ethics-and-privacy" 
+        "MP.3"    ; STEMworks
+        )
+
+    ("grouped-samples"
+        )
+
+    ("if-expressions" 
+        )
+
+    ("histograms" 
+        "MP.4" ; STEMworks
+        )
+
+    ("histograms2" 
+        "MP.4" ; STEMworks
+        )
+
+    ("linearity"
+        "MP.1"
+        "MP.2"
+        "MP.3"
+        "MP.5"
+        "MP.7"
+        )
+
+    ("linearity2"
+        "MP.1"
+        "MP.2"
+        "MP.3"
+        "MP.5"
+        "MP.7"
+        )
+
+    ("linear-regression" 
+        "MP.4" ; STEMworks
+        )
+
+    ("measures-of-center"
+        "MP.3" ; STEMworks
+        )
+
+    ("measures-of-spread" 
+        "MP.3" ; STEMworks
+        "MP.4" ; STEMworks
+        )
+
+    ("method-chaining" 
+
+        )
+
+    ("displaying-categorical-data"  
+
+        )
+
+    ("random-samples" 
+
+        )
+
+    ("scatter-plots"
+
+        )
+
+    ("table-methods" 
+
+        )
+
+    ("threats-to-validity" 
+        "MP.3" ; STEMworks
+        )
+    ))
