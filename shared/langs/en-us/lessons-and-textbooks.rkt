724d77f1b8702e12768fa99740f5c702adce2482
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2021-07-20 10:26:43 -0400

    [lib] add lessons-and-textbooks file

---
+++ shared/langs/en-us/lessons-and-textbooks.rkt
@@ -0,0 +1,180 @@
+#lang racket
+
+;FIXME what wd be the canonical location of this file?
+;      the pathway top dir?
+
+(provide *lessons-and-standards*)
+
+(define *lessons-and-standards*
+  '(
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; CORE
+
+    ("contracts" 
+        )
+
+    ("defining-functions" 
+        )
+
+    ("defining-values"
+        "CMP.6.6" 
+        )
+
+    ("simple-dataypes" 
+        ) 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; MATH
+   
+    ("combinatorics-permutation"
+        "HSS.CP.B.9"
+        "OK.SP.1.5"
+        )
+
+    ("combinatorics-combination"
+        "HSS.CP.B.9"
+        "OK.SP.1.5"
+        )
+
+    ("coordinates"
+        "IA.5.G.A.1" 
+        "5.G.A.1"
+        )
+
+    ("distance-formula" 
+        "CMP.8.2"
+        )
+
+    ("flags" 
+        ) 
+
+    ("function-composition" 
+        )
+
+    ("functions-for-character-animation" 
+        )
+
+    ("inequalities-collision"
+        )
+
+    ("inequalities1-simple"
+        )
+
+    ("inequalities2-compound" 
+        )
+
+    ("inequalities3-sam"
+        )    
+
+    ("making-game-images" 
+        )
+
+    ("numbers-inside-videogames" 
+        )
+
+    ("order-of-operations" 
+        "CMP.6.1"
+        "CMP.7.2"
+        )
+
+    ("piecewise-functions" 
+        "CMP.Alg-Ext.2"
+        )
+
+    ("player-animation" 
+        )      
+
+    ("problem-decomposition"
+        )
+        
+    ("restating-the-problem" 
+        )
+
+    ("solving-word-problems" 
+        )
+
+    ("surface-area-rect-prism"
+        "CMP.6.4"
+        "CMP.7.7"
+        )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; DATA SCIENCE
+    
+    ("checking-your-work" 
+        )
+
+    ("choosing-your-dataset" 
+        )
+
+    ("correlations" 
+        "CMP.8.1"
+        )
+    
+      ("defining-table-functions" 
+        )
+
+    ("displays-and-lookups" 
+        )
+
+    ("ds-intro" 
+        )
+
+    ("ethics-and-privacy" 
+        )
+
+    ("grouped-samples"
+        )
+
+    ("if-expressions" 
+        )
+
+    ("histograms" 
+        )
+
+    ("histograms2" 
+        )
+
+    ("linearity"
+        "CMP.7.5"
+        "CMP.8.1"
+        )
+
+    ("linearity2"
+        "CMP.7.5"
+        "CMP.8.1"
+        )
+
+    ("linear-regression" 
+        "CMP.8.1"
+        )
+
+    ("measures-of-center"
+        "CMP.6.7"
+        "CMP.8.1"
+        )
+
+    ("measures-of-spread" 
+        "CMP.6.7"
+        )
+
+    ("method-chaining" 
+        )
+
+    ("displaying-categorical-data" 
+        )
+
+    ("random-samples" 
+        "CMP.7.8"
+        )
+
+    ("scatter-plots"
+        "CMP.8.1"
+        )
+
+    ("table-methods" 
+        )
+
+    ("threats-to-validity" 
+        )
+    ))
