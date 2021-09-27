2a6a1ce4e6b0294fb4e21dd4d77cdce0ca1348ee
Dorai Sitaram <ds26gte@gmail.com>
2021-09-27 16:10:00 -0400

    Revert "Jsmakeworkbook (#608)"
    This reverts commit 5ac3e6e0bf304fa95a75cff0c74db351c169e58e.

--- lib/preproc.rkt
+++ lib/preproc.rkt
@@ -104,7 +104,7 @@
       (let ([f (string-append *pathway-root-dir* ".cached/.workbook-page-index.rkt")])
         (if (file-exists? f)
             (let ([xx (call-with-input-file f read)])
-              (map (lambda (x) (list (list (list-ref x 0) (list-ref x 1)) (list-ref x 3))) xx)
+              (map (lambda (x) (list (list (list-ref x 0) (list-ref x 1)) (list-ref x 4))) xx)
               )
             '()))
       '()))
@@ -440,18 +440,14 @@
   (fprintf o "\n\ninclude::{cachedir}.pathway-glossary.asc[]\n\n"))
 
 (define (workbook-pagenum lesson snippet)
-  ;(printf "doing workbook-pagenum ~s ~s\n" lesson snippet)
+  ;(printf "workbook-pagenum ~s ~s\n" lesson snippet)
   ;(printf "*workbook-pagenums* = ~s\n" *workbook-pagenums*)
-  #|
   (let* ([snippet.adoc
            (path->string
              (path-replace-extension snippet ".adoc"))]
          [c (or (assoc (list lesson snippet.adoc) *workbook-pagenums*)
                 (assoc (list lesson snippet) *workbook-pagenums*))])
-    (if c (cadr c) #f))
-  |#
-  #t
-  )
+    (if c (cadr c) #f)))
 
 (define (exercise-title f)
   (if (and (file-exists? f) (path-has-extension? f ".adoc"))
@@ -509,7 +505,6 @@
                     link-text
                     (if *lesson-plan*
                         (let ([pagenum (workbook-pagenum lesson snippet)])
-                          ;(printf "link-type= ~s; nopdf= ~s; book= ~s; error-cascase= ~s\n" link-type *nopdf* *book* error-cascade?)
                           (unless (or (equal? link-type "opt-printable-exercise")
                                       *nopdf*
                                       (not *book*)
@@ -1695,7 +1690,7 @@
               (fprintf op "~s~n" (car s))
               (let ([x (caddr s)])
                 (cond [first? (set! first? #f)]
-                      [else (display ",\n                " op2)])
+                      [else (display ", " op2)])
                 (write (string-append (car x) " : " (cadr x)) op2))
               )
             (display "]" op2) (newline op2)
