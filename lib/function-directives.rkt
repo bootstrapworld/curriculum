59287613cda14b241006e61616153875a67f9bbc
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2021-10-07 13:15:48 -0400

    Smarter fill-in-the-blanks (#617)
    This PR introduces display:flex to our repetoire for styling fill in the blanks. For now, its use is limited to Design Recipes only, as it addresses #228 . Immediately, this results in all blanks growing to fit the available horizontal space.
    
    Over time, we can expand this use into other worksheets, replacing brittle @fitb{XYZ} with @fitb{flex} to show that a blank should extend as far as possible. This makes maintenance a lot easier in the long run, since blanks "know how to stretch themselves".

--- lib/function-directives.rkt
+++ lib/function-directives.rkt
@@ -266,9 +266,9 @@
 
 (define (write-title title-text)
   (string-append
-    "\n\n[.recipe_title, cols=\"1a\"]\n"
+    "\n\n[.recipe_title, cols=\"100a,1a\"]\n"
     "|===\n"
-    "| " title-text "\n"
+    "| " title-text "|\n"
     "|===\n\n"))
 
 (define (write-purpose funname domain-list range purpose)
@@ -277,7 +277,6 @@
     "[.recipe.recipe_instructions]\n"
     "Every contract has three parts...\n\n"
     "[.recipe.recipe_contract]\n"
-    (if *pyret?* "&#x23; " "&#x3b; ")
     (encoded-ans ".recipe_name" funname *show-funname-contract?*)
     (if *pyret?* "::" ":")
     (encoded-ans ".recipe_domain"
@@ -288,8 +287,6 @@
     (encoded-ans ".recipe_range" range *show-range?*)
     "\n\n"
     "[.recipe.recipe_purpose_statement]\n"
-    (enclose-span ".purpose_comment" (if *pyret?* "#" ";"))
-    " "
     (write-purpose-prose purpose)))
 
 (define (write-purpose-prose purpose)
@@ -381,34 +378,16 @@
     (write-wrapper ".recipe.recipe_example_line"
       (lambda ()
         (string-append
-          ;(write-clear)
-          (write-large "(")
-          (write-spaced "EXAMPLE ")
-          (write-large "(")
+          (encoded-ans "" "(EXAMPLE (" #t)
           (encoded-ans ".recipe_name" funname show-funname?)
           " "
           (encoded-ans ".recipe_example_inputs" (list-to-string args) show-args?)
-          (write-large ")")
+          (encoded-ans "" ")" #t)
           (let ([body-s (expr-to-string body)])
-            (if (longer-than? *max-line-length*
-                               body-s (expr-to-string funname) (list-to-string args))
-                (begin
-                  ;(printf "maxlinelen exceeded\n")
-                  (string-append (write-clear)
-                    (encoded-ans "" "MM" #f)
-                    (encoded-ans ".recipe_example_body_long"
-                                 (encoded-ans "" body-s show-body?)
-                                 #t)
-                    (write-large ")")))
-                (begin
-                  ;(printf "within maxlinelen\n")
-                  (string-append
-                    (enclose-span ""
-                      (string-append
-                        (encoded-ans "" "M" #f)
-                        (encoded-ans ".recipe_example_body_short" body-s show-body?)))
-                    (write-large ")")
-                    ))))
+            (string-append
+              (encoded-ans ".recipe_example_body" body-s show-body?)
+              (write-large ")")
+                  ))
             )))))
 
 (define (write-each-example/pyret funname show-funname? args show-args? body show-body?)
@@ -434,28 +413,9 @@
         (write-large "(")
         (encoded-ans ".recipe_example_inputs" args show-args?)
         (write-large ")")
-        (highlight-keywords " is")
-        ; wrap the `is ...` code in its own element, so that wrapping works properly
-
-        (if (longer-than? *max-line-length* body funname args)
-            (begin
-              ;(printf "maxlinelen exceeded\n")
-              (string-append (write-clear)
-                (encoded-ans "" "MMMM" #f)
-                (encoded-ans ".recipe_example_body_long"
-                             (string-append
-                               ;(highlight-keywords "is ")
-                               ;(encoded-ans "" "MM" #f)
-                               (encoded-ans "" (highlight-keywords body) show-body?)) #t)))
-            (begin
-              ;(printf "within maxlinelen\n")
-              (enclose-span ""
-                (string-append
-                  ;(highlight-keywords "is ")
-                  (encoded-ans "" "M" #f)
-                  (encoded-ans ".recipe_example_body_short" (highlight-keywords body) show-body?)
-                  ))))
-        (write-clear)
+        (encoded-ans "" "__" #f)
+        (highlight-keywords " is ")
+        (encoded-ans ".recipe_example_body" (highlight-keywords body) show-body?)
         ))))
 
 (define (write-each-example funname args body show)
@@ -523,9 +483,7 @@
               (lambda ()
                 (string-append
                   (write-large "(")
-                  (write-spaced "define")
-                  (write-spaced " ")
-                  (write-large "(")
+                  (write-spaced "define (")
                   (encoded-ans ".recipe_name" funname *show-funname-defn?*)
                   " "
                   (encoded-ans ".recipe_variables" (vars-to-string param-list) *show-params?*)
@@ -537,7 +495,8 @@
                         (string-append
                           (encoded-ans "" "__" #f)
                           (write-large "(")
-                          (encoded-ans ".recipe_cond" "cond" *show-body?*))))]
+                          (encoded-ans ".recipe_cond" "cond" *show-body?*)
+                          (encoded-ans "" "__" #f))))]
                   [else ""])
 
             (cond [cond?
@@ -557,7 +516,7 @@
                       (lambda ()
                         (string-append
                           (encoded-ans "" "__" #f)
-                          (write-large "))" #:tag ".studentAnswer.recipe_definition_body"))))]
+                          (write-large "))" #:tag ".studentAnswer"))))]
                   [else
                     ""
                     #|(write-wrapper ".recipe.recipe_line"
@@ -635,8 +594,7 @@
                       (encoded-ans ".recipe_name" funname *show-funname-defn?*)
                       (write-large "(")
                       (encoded-ans ".recipe_variables" (vars-to-commaed-string param-list) *show-params?*)
-                      (write-large ")")
-                      (write-spaced ":"))))
+                      (write-large "):"))))
                 (apply string-append
                        (map write-body-line/pyret but-last-body-lines))
                 (write-wrapper ".recipe.recipe_line"
@@ -668,7 +626,7 @@
   )
 
 (define (write-large s #:tag [tag ""])
-  (enclose-span (string-append ".bigparen" tag) s))
+  (enclose-span (string-append ".studentAnswerCode" tag) s))
 
 (define *wrapper-block-level* 0)
 
