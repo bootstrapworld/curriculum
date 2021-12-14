ca4dcbe2639c6da5fef8df0710698b4affb8c128
Dorai Sitaram <ds26gte@gmail.com>
2021-12-14 17:56:43 -0500

    Create bilingual-glossary.html in distribution/en-us/lib. (Added lib/make-bilingual-glossary.rkt)

---
+++ lib/make-bilingual-glossary.rkt
@@ -0,0 +1,35 @@
+#!/usr/bin/env racket
+
+#lang racket
+
+(require "glossary-terms.rkt")
+
+; assumes glossary is given in alpha order for English at least
+
+(call-with-output-file "bilingual-glossary.adoc"
+  (lambda (o)
+    (fprintf o "= Bilingual Glossary\n\n")
+    (fprintf o "++++\n")
+    (fprintf o "<style>\n")
+    (fprintf o ".missing { font-style: italic; color: #993333; }\n")
+    (fprintf o "</style>\n")
+    (fprintf o "++++\n\n")
+
+    (for ([entry *glossary-list*])
+      ; (printf "entry = ~s\n" entry)
+      (let ([en-entry (assoc 'en-us entry)]
+            [es-entry (assoc 'es-mx entry)])
+        (if en-entry
+            (let* ([kwds (cadr en-entry)]
+                   [kwd (if (list? kwds) (string-join kwds ", ") kwds)]
+                   [desc (caddr en-entry)])
+              (fprintf o "~a :: ~a\n\n" kwd desc))
+            (fprintf o "[.missing]#missing entry# :: ...\n\n"))
+        (if es-entry
+            (let* ([kwds (cadr es-entry)]
+                   [kwd (if (list? kwds) (string-join (cadr es-entry) ", ") kwds)]
+                   [desc (caddr es-entry)])
+              (fprintf o "~a :: ~a\n\n" kwd desc))
+            (fprintf o "[.missing]#missing entry# :: ...\n\n"))
+        (fprintf o "'''\n\n"))))
+  #:exists 'replace)
