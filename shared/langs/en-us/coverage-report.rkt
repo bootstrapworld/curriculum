83975823eb4472313f45853b1c37217ef3c49fa0
Dorai Sitaram <ds26gte@gmail.com>
2021-12-21 10:45:46 -0500

    coverage-report: Add column showing text associated with standard. Correct pathnames to reflect new location of standards files.

--- shared/langs/en-us/coverage-report.rkt
+++ shared/langs/en-us/coverage-report.rkt
@@ -2,15 +2,15 @@
 
 #lang racket
 
-(require "standards-cc-ela-dictionary.rkt")
-(require "standards-cc-math-dictionary.rkt")
-(require "standards-csta-dictionary.rkt")
-(require "standards-k12cs-dictionary.rkt")
-(require "standards-ngss-dictionary.rkt")
-(require "standards-ok-dictionary.rkt")
-(require "standards-ia-dictionary.rkt")
+(require "standards/standards-cc-ela-dictionary.rkt")
+(require "standards/standards-cc-math-dictionary.rkt")
+(require "standards/standards-csta-dictionary.rkt")
+(require "standards/standards-k12cs-dictionary.rkt")
+(require "standards/standards-ngss-dictionary.rkt")
+(require "standards/standards-ok-dictionary.rkt")
+(require "standards/standards-ia-dictionary.rkt")
 
-(require "lessons-and-standards.rkt")
+(require "standards/lessons-and-standards.rkt")
 
 (define *all-the-standards* '())
 
@@ -23,38 +23,42 @@
                       *iowa-standards-list*
                       )])
   (for ([std-desc std-list])
-    (let ([std (car std-desc)])
-      (set! *all-the-standards*
-        (cons (list std (box 0)) *all-the-standards*)))))
+    (set! *all-the-standards*
+      (cons (list (car std-desc) (cadr std-desc) (box 0)) *all-the-standards*))))
 
 (for ([lesson-line *lessons-and-standards*])
   (let ([lesson-stds (cdr lesson-line)])
     (for ([std lesson-stds])
       (let ([c (assoc std *all-the-standards*)])
         (when c
-          (let ([b (cadr c)])
+          (let ([b (caddr c)])
             (set-box! b (+ (unbox b) 1))))))))
 
 (call-with-output-file "coverage-report.adoc"
   (lambda (o)
     (fprintf o "= Coverage of Standards\n\n")
 
-    (fprintf o "Each line shows the name of the standard followed by the number of times it's used. ")
+    (fprintf
+      o "Each line shows the name of the standard followed by the number of times it's used.\n")
     (fprintf o "If a standard is not used at all, the line is highlighted in red.\n\n")
 
     (fprintf o "++++\n")
     (fprintf o "<style>\n")
-    (fprintf o ".unused { background-color: darkred; color: white; }\n")
+    (fprintf o ".unused { background-color: #ffdddd; }\n")
     (fprintf o "</style>\n")
     (fprintf o "++++\n\n")
 
-    (fprintf o "[verse]\n")
-    (for ([std-desc *all-the-standards*])
-      (let ([std (car std-desc)] [count (unbox (cadr std-desc))])
+    (fprintf o "[cols=\"2a,1a,7a\"]\n")
+    (fprintf o "|===\n")
+    (for ([entry *all-the-standards*])
+      (let ([std (car entry)] [desc (cadr entry)] [count (unbox (caddr entry))])
+        (set! desc (regexp-replace* "\\|" desc "\\&#x7c;"))
         (if (= count 0)
-            (fprintf o "[.unused]#~a \t ~a#\n" std count)
-            (fprintf o "~a \t ~a\n" std count))))
-    (fprintf o "\n\n")
+            (fprintf o "| [.unused]#~a# | [.unused]#~a# | [.unused]#~a#\n" std count desc)
+            (fprintf o "| ~a | ~a | ~a\n" std count desc))))
+    (fprintf o "|===\n\n")
+
     )
+
   #:exists 'replace)
 
