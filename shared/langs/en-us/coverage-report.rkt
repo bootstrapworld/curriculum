5e598a69a2485ac176e13c1dddfee9f38369a3ad
Dorai Sitaram <ds26gte@gmail.com>
2022-01-05 12:52:24 -0500

    coverage report should include subsections for Practices & Textbooks

--- shared/langs/en-us/coverage-report.rkt
+++ shared/langs/en-us/coverage-report.rkt
@@ -2,6 +2,8 @@
 
 #lang racket
 
+;standards
+
 (require "standards/standards-cc-ela-dictionary.rkt")
 (require "standards/standards-cc-math-dictionary.rkt")
 (require "standards/standards-csta-dictionary.rkt")
@@ -12,34 +14,61 @@
 
 (require "standards/lessons-and-standards.rkt")
 
-(define *all-the-standards* '())
+;practices
 
-(for ([std-list (list *cc-ela-standards-list*
-                      *cc-math-standards-list*
-                      *csta-standards-list*
-                      *k12cs-standards-list*
-                      *ngss-standards-list*
-                      *ok-standards-list*
-                      *iowa-standards-list*
-                      )])
-  (for ([std-desc std-list])
-    (set! *all-the-standards*
-      (cons (list (car std-desc) (cadr std-desc) (box 0)) *all-the-standards*))))
+(require "practices/practices-mlr-dictionary.rkt")
+(require "practices/practices-sj-dictionary.rkt")
+(require "practices/practices-sep-dictionary.rkt")
+(require "practices/practices-cc-dictionary.rkt")
 
-(for ([lesson-line *lessons-and-standards*])
-  (let ([lesson-stds (cdr lesson-line)])
-    (for ([std lesson-stds])
-      (let ([c (assoc std *all-the-standards*)])
-        (when c
-          (let ([b (caddr c)])
-            (set-box! b (+ (unbox b) 1))))))))
+(require "practices/lessons-and-practices.rkt")
+
+;textbooks
+
+(require "textbooks/textbook-cmp-dictionary.rkt")
+(require "textbooks/textbook-im-6-dictionary.rkt")
+(require "textbooks/textbook-im-7-dictionary.rkt")
+(require "textbooks/textbook-im-8-dictionary.rkt")
+(require "textbooks/textbook-im-alg-1-dictionary.rkt")
+
+(require "textbooks/lessons-and-textbooks.rkt")
+
+;;;;;;;;;;;;;
+
+(define (display-subreport o title lesson-entries list-of-alists)
+  (define *all-the-values* '())
+
+  (for ([lyst list-of-alists])
+    (for ([desc lyst])
+      (set! *all-the-values*
+        (cons (list (car desc) (cadr desc) (box 0)) *all-the-values*))))
+
+  (for ([lesson-entry lesson-entries])
+    (let ([lesson-vals (cdr lesson-entry)])
+      (for ([val lesson-vals])
+        (let ([c (assoc val *all-the-values*)])
+          (when c
+            (let ([b (caddr c)])
+              (set-box! b (+ (unbox b) 1))))))))
+
+  (fprintf o "== ~a\n\n" title)
+
+    (fprintf o "[cols=\"2a,1a,7a\"]\n")
+    (fprintf o "|===\n")
+    (for ([entry *all-the-values*])
+      (let ([std (car entry)] [desc (cadr entry)] [count (unbox (caddr entry))])
+        (set! desc (regexp-replace* "\\|" desc "\\&#x7c;"))
+        (if (= count 0)
+            (fprintf o "| [.unused]#~a# | [.unused]#~a# | [.unused]#~a#\n" std count desc)
+            (fprintf o "| ~a | ~a | ~a\n" std count desc))))
+    (fprintf o "|===\n\n"))
 
 (call-with-output-file "coverage-report.adoc"
   (lambda (o)
-    (fprintf o "= Coverage of Standards\n\n")
+    (fprintf o "= Coverage Report\n\n")
 
     (fprintf
-      o "Each line shows the name of the standard followed by the number of times it's used.\n")
+      o "Each line shows the name of the standard/practice/textbook followed by the number of times it's used.\n")
     (fprintf o "If a standard is not used at all, the line is highlighted in red.\n\n")
 
     (fprintf o "++++\n")
@@ -48,16 +77,28 @@
     (fprintf o "</style>\n")
     (fprintf o "++++\n\n")
 
-    (fprintf o "[cols=\"2a,1a,7a\"]\n")
-    (fprintf o "|===\n")
-    (for ([entry *all-the-standards*])
-      (let ([std (car entry)] [desc (cadr entry)] [count (unbox (caddr entry))])
-        (set! desc (regexp-replace* "\\|" desc "\\&#x7c;"))
-        (if (= count 0)
-            (fprintf o "| [.unused]#~a# | [.unused]#~a# | [.unused]#~a#\n" std count desc)
-            (fprintf o "| ~a | ~a | ~a\n" std count desc))))
-    (fprintf o "|===\n\n")
+    (display-subreport o "Standards" *lessons-and-standards*
+                       (list *cc-ela-standards-list*
+                             *cc-math-standards-list*
+                             *csta-standards-list*
+                             *k12cs-standards-list*
+                             *ngss-standards-list*
+                             *ok-standards-list*
+                             *iowa-standards-list*
+                             ))
 
+    (display-subreport o "Practices" *lessons-and-practices*
+                       (list *mlr-practices-list*
+                             *sj-practices-list*
+                             *sep-practices-list*
+                             *cc-practices-list*))
+
+    (display-subreport o "Textbooks" *lessons-and-textbooks*
+                       (list *cmp-textbook-list*
+                             *im-6-textbook-list*
+                             *im-7-textbook-list*
+                             *im-8-textbook-list*
+                             *im-alg-1-textbook-list*))
     )
 
   #:exists 'replace)
