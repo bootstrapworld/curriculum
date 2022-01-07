f9216f799008a4d19944d82d9ad34b2b947f4753
Dorai Sitaram <ds26gte@gmail.com>
2022-01-07 15:06:34 -0500

    Coverage report uses dropdown for standard categories (grouped under Standards), textbook categories (grouped under Textbooks), and (the single) practice category.

--- shared/langs/en-us/coverage-report.rkt
+++ shared/langs/en-us/coverage-report.rkt
@@ -24,37 +24,65 @@
 
 ;;;;;;;;;;;;;
 
-(define (display-subreport o title lesson-entries dictionaries) ; list-of-alists
+(define (sanitize-css-id id)
+  (regexp-replace* "\\." id "_"))
+
+(define (display-selection o )
+  (display "\n++++\n" o)
+  (display "<select class=\"coverageAlignmentSelect\" onchange=\"showCoverageAlignment()\">\n" o)
+  (display "<optgroup label=\"Standards\">\n" o)
+  (for ([categ *standards-list*])
+    (fprintf o "<option>~a</option>\n" (sanitize-css-id (car categ)))
+    )
+  (display "</optgroup>\n" o)
+  ;
+  (display "<optgroup label=\"Textbooks\">\n" o)
+  (for ([categ *textbooks-list*])
+    (fprintf o "<option>~a</option>\n" (sanitize-css-id (car categ))))
+  (display "</optgroup>\n" o)
+  ;
+  (display "<optgroup label=\"Practices\">\n" o)
+  (for ([categ *practices-list*])
+    (fprintf o "<option>~a</option>\n" (sanitize-css-id (car categ))))
+  (display "</optgroup>\n" o)
+  (display "</select>\n" o)
+  (display "++++\n" o))
+
+(define (display-subreport o title lesson-entries dictionaries)
   ; (printf "doing display-subreport ~s  \n" title  )
-  (define *all-the-values* '())
 
   (for ([dictionary dictionaries])
     ; (printf "doing dictionary ~s\n" dictionary)
-    (let ([lyst (list-ref dictionary (- (length dictionary) 1))])
-      ; (printf "doing lyst = ~s\n" lyst)
+    (let ([lyst (list-ref dictionary (- (length dictionary) 1))]
+          [opt (sanitize-css-id (car dictionary))]
+          [counters '()])
+      ; (printf "doing lyst = ~s\n" opt)
       (for ([desc lyst])
-        (set! *all-the-values*
-          (cons (list (car desc) (cadr desc) (box 0)) *all-the-values*)))))
+        (set! counters
+          (cons (list (car desc) (cadr desc) (box 0)) counters)))
 
-  (for ([lesson-entry lesson-entries])
-    (let ([lesson-vals (cdr lesson-entry)])
-      (for ([val lesson-vals])
-        (let ([c (assoc val *all-the-values*)])
-          (when c
-            (let ([b (caddr c)])
-              (set-box! b (+ (unbox b) 1))))))))
+      (set! counters (reverse counters))
 
-  (fprintf o "== ~a\n\n" title)
+      (for ([lesson-entry lesson-entries])
+        (let ([lesson-vals (cdr lesson-entry)])
+          (for ([val lesson-vals])
+            (let ([c (assoc val counters)])
+              (when c
+                (let ([b (caddr c)])
+                  (set-box! b (+ (unbox b) 1))))))))
 
-    (fprintf o "[cols=\"2a,1a,7a\"]\n")
-    (fprintf o "|===\n")
-    (for ([entry *all-the-values*])
-      (let ([std (car entry)] [desc (cadr entry)] [count (unbox (caddr entry))])
-        (set! desc (regexp-replace* "\\|" desc "\\&#x7c;"))
-        (if (= count 0)
-            (fprintf o "| [.unused]#~a# | [.unused]#~a# | [.unused]#~a#\n" std count desc)
-            (fprintf o "| ~a | ~a | ~a\n" std count desc))))
-    (fprintf o "|===\n\n"))
+      (fprintf o "[.coverageElement.~a]\n" opt)
+      (fprintf o "[cols=\"2a,1a,7a\"]\n")
+      (fprintf o "|===\n")
+      (for ([entry counters])
+        (let ([std (car entry)] [desc (cadr entry)] [count (unbox (caddr entry))])
+          (set! desc (regexp-replace* "\\|" desc "\\&#x7c;"))
+          (if (= count 0)
+              (fprintf o "| [.unused]#~a# | [.unused]#~a# | [.unused]#~a#\n" std count desc)
+              (fprintf o "| ~a | ~a | ~a\n" std count desc)))
+        )
+      (fprintf o "|===\n\n")
+      )))
 
 (call-with-output-file "coverage-report.adoc"
   (lambda (o)
@@ -65,6 +93,7 @@
     (fprintf o "If a standard is not used at all, the line is highlighted in red.\n\n")
 
     (print-coverage-script-n-style o)
+    (display-selection o)
 
     (display-subreport o "Standards" *lessons-and-standards* *standards-list*)
 
