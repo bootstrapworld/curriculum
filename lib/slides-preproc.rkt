c470d0833faf6e54ae08b4b80156c77f5c03b43f
Dorai Sitaram <ds26gte@gmail.com>
2022-05-11 12:01:32 -0400

    Slap AUTO-GEN: in front of slides title, indicating postproc not done

--- lib/slides-preproc.rkt
+++ lib/slides-preproc.rkt
@@ -188,13 +188,19 @@
 
 (define (preproc-slides-file in-file)
   (set! *in-file* in-file)
-  (let ([out-file (path-replace-extension in-file ".mkd")])
+  (let ([out-file (path-replace-extension in-file ".mkd")]
+        [title-read? #f])
 
     (define (expand-directives i o)
       (let loop ()
         (let ([c (read-char i)])
           (unless (eof-object? c)
             (cond
+              [(char=? c #\#)
+               (display c o)
+               (unless title-read?
+                 (set! title-read? #t)
+                 (display " AUTO-GEN:" o))]
               [(char=? c #\@)
                (let ([directive (read-word i)])
                  ; (printf "directive is ~s\n" directive)
