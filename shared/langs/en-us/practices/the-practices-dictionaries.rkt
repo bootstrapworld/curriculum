7de24126017af563174a1b52acbb5e85def62072
Dorai Sitaram <ds26gte@gmail.com>
2022-05-19 00:15:50 -0400

    - Replace c[ad]*r with {first, second, ..., rest}. - build-pathway reminder for npm update (rather than npm install)

--- shared/langs/en-us/practices/the-practices-dictionaries.rkt
+++ shared/langs/en-us/practices/the-practices-dictionaries.rkt
@@ -23,7 +23,7 @@
     (list "CC" "Common Core Practice Standards" *cc-practices-list* "http://corestandards.org/Math/")
     (list "K12CS" "K12CS Practices" *k12cs-practices-list* "https://k12cs.org")))
 
-(define *flat-practices-list* (apply append (map caddr *practices-list*)))
+(define *flat-practices-list* (apply append (map third *practices-list*)))
 
 (define (expand-practice-abbrev pce)
   (let ([x (assoc pce *practices-list*)])
