e2c7b4e3fac8f00054f2bbe87be4e12165db19c3
Dorai Sitaram <ds26gte@gmail.com>
2022-06-22 02:54:50 -0400

    - `code` should pass multi-line kwd arg to `sexp->code` (and trickle it all the way thru). - sexp->arith in pyret-mode should handle EXAMPLE. #986

--- lib/html-tag-gen.rkt
+++ lib/html-tag-gen.rkt
@@ -64,7 +64,8 @@
   ;(printf "doing enclose-textarea-2 ~s\n" s)
   (set! classes ".codetwo")
   (let ([ta (enclose-tag "span" classes s)])
-    ta))
+    (if multi-line (enclose-span ".obeyspaces" ta)
+        ta)))
 
 (define (low-quality-math x)
   (set! x (regexp-replace* "\\\\;" x ""))
