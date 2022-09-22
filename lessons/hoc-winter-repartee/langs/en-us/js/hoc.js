09bae75df90bc7a65275dbdb048d253f22995e44
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2022-09-22 15:59:36 -0400

    [hoc] repartee cleanup

--- lessons/hoc-winter-repartee/langs/en-us/js/hoc.js
+++ lessons/hoc-winter-repartee/langs/en-us/js/hoc.js
@@ -1,137 +1,58 @@
-function initializeCards(){
-  var is_firefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
-  if(is_firefox){
-//    alert("Due to a bug in Firefox, this tutorial is best used in Chrome, Internet Explorer or Safari. We're working to fix this as soon as possible.");
-  }
-  window.cardIndex = 0;                                   // the index
-  window.slider = document.getElementById('cards');        // the slider that gets moved
-  window.progress=document.getElementById('progressbar');  // the progressbar
-  window.cards = [].slice.call(slider.children);          // an array of all the cards
-  document.getElementById('next').style.display = "block"; // enable the "next" button
-  showRightHand();
-  loadRightHand(1);
+let cardIndex, slider, progress, cards, editor, parley, righthand;
+
+window.onload = () => {
+  cardIndex = 0;                                        // the index
+  slider    = document.getElementById('cards');         // the slider that gets moved
+  progress  = document.getElementById('progressbar');   // the progressbar
+  cards     = [].slice.call(slider.children);           // an array of all the cards
+  editor    = document.createElement("div");
+  parley    = window.embedableParley.renderParley(editor);
+  righthand = document.getElementById("righthand");
+  loadFromConfig(cardIndex);
+  updateSliderAndButtons();
   progress.style.width = (cardIndex+1) * (100 / cards.length) + "%";
 }
 
-function nextCard(){
-  // hide current content before loading the next content
-  if(cards[cardIndex].content){
-    cards[cardIndex].content.style.visibility = "hidden";
-  }
-  console.log('loading next card: '+(window.cardIndex+1));
-  cardIndex++;
-  if(cardIndex >= cards.length-1) document.getElementById('next').style.display = "none";
-  if(cardIndex > 0) document.getElementById('prev').style.display = "block";
-  showRightHand();
+function updateSliderAndButtons() {
+  document.getElementById('prev').style.display = (cardIndex === 0)? "none" : "block"
+  document.getElementById('next').style.display = (cardIndex >= cards.length-1)? "none" : "block";
   slider.style.left = (cardIndex*-400) + "px";
   slider.scrollIntoView();
-  setTimeout(function(){loadRightHand(cardIndex+1);}, 1000); // preload the next card's content
   progress.style.width = (cardIndex+1) * (100 / cards.length) + "%";
+  loadFromConfig(cardIndex);
 }
 
-function previousCard(){
-  // hide current content before loading the previous content
-  if(cards[cardIndex].content){
-    cards[cardIndex].content.style.visibility = "hidden";
-  }
-  console.log('loading previous card: '+(window.cardIndex-1));
+function nextCard() {
+  cardIndex++;
+  updateSliderAndButtons();
+}
+
+function previousCard() {
   cardIndex--;
-  if(cardIndex < cards.length) document.getElementById('next').style.display = "block";
-  if(cardIndex === 0) document.getElementById('prev').style.display = "none";
-  showRightHand();
-  slider.style.left = (cardIndex*-400) + "px";
-  loadRightHand(cardIndex-1); // preload the next card's content
-  progress.style.width = (cardIndex+1) * (100 / cards.length) + "%";
-  if(cards[cardIndex].content) cards[cardIndex].content.style.visibility = "visible";
+  updateSliderAndButtons();
 }
 
-function showRightHand(){
-  loadRightHand(cardIndex); // make sure it's loaded
-  if(cards[cardIndex].content) cards[cardIndex].content.style.visibility = "visible";
-}
+function loadFromConfig(i = cardIndex) {
+  if(i > cards.length-1) return; // if there's no more cards, bail
 
-function loadRightHand(i){
-  if(cards[i] && cards[i].content){
-    console.log('loading from cache!');
-    return;
-  }
-  var imageConfig = cards[i].getAttribute('imageConfig');
-  if(cards[i].hasAttribute('weschemeConfig')) {
-    var weschemeConfig = cards[i].getAttribute('weschemeConfig');
-    var url = "https://www.WeScheme.org/openEditor?warnOnExit=false&hideHeader=true&hideToolbar=false&hideProjectName=true&hideFooter=true&name=embedded&blocksMode=true";
-    if(weschemeConfig !== null){  // if it's non-null, we need to escape special chars with encodeURIComponent
-      weschemeConfig = encodeURIComponent(weschemeConfig);
-      url += "&definitionsText="+weschemeConfig;
-    }
-    console.log('loading embedded WeScheme with the following URL', url);
-    cards[i].content = WeSchemeEmbedded.withoutRpc(url, "righthand");
-  } else if(imageConfig){
+  // clear any content
+  while(righthand.firstChild) { righthand.removeChild(righthand.firstChild); }
+
+  const editorConfig= cards[i].getAttribute('editorCode'),
+        imageConfig = cards[i].getAttribute('imageConfig');
+
+  if(editorConfig) {
+    righthand.appendChild(editor);
+    parley.resetChunks(editorConfig.trim().split("\n") || [""]);
+  } else if(imageConfig) {
     var img = document.createElement("img");
     img.src = imageConfig;
-    document.getElementById("righthand").appendChild(img);
-    cards[i].content = img;
-  }
-  if(cards[i].content) cards[i].content.style.visibility = "hidden";
-  console.log('content loaded and cached for card '+i);
-}
-
-// forEach method, could be shipped as part of an Object Literal/Module
-var forEach = function (array, callback, scope) {
-  for (var i = 0; i < array.length; i++) {
-    callback.call(scope, i, array[i]); // passes back stuff we need
-  }
-};
-
-function attachCodeMirror(){
-  // translate all the TEXTAREA nodes into full-blown CM instances, and color TTs with runmode
-  // (we use the more expensive CM stuff for indenting)
-  var codeSnippets = document.querySelectorAll('textarea');
-  forEach(codeSnippets, function(idx, textarea){
-          if(textarea.className == "racket"){
-          CodeMirror.fromTextArea(textarea, {mode:"scheme2", readOnly: "nocursor"});
-          } else if(textarea.className == "pyret"){
-          CodeMirror.fromTextArea(textarea, {mode:"pyret", readOnly: "nocursor"});
-          }
-          });
-  
-  var codeSnippets = document.querySelectorAll('tt');
-  forEach(codeSnippets, function(idx, tt){
-          var node = document.createElement('span');
-          node.className = 'editbox';
-          tt.parentNode.insertBefore(node, tt);
-          if(tt.className == "racket"){
-          CodeMirror.runMode(tt.firstChild.nodeValue, "scheme2", node);
-          } else if(tt.className == "pyret"){
-          CodeMirror.runMode(tt.firstChild.nodeValue, "pyret", node);
-          } else {
-          console.log('<tt> declared without a language className');
-          }
-          tt.style.display = 'none';
-          });
-}
-
-/************ VIDEO LIGHTBOX *********************/
-window.document.onkeydown = function(e) {
-  if (!e) {
-    e = event;
-  }
-  if (e.keyCode == 27) {
-    lightbox_close();
+    righthand.appendChild(img);
   }
 }
 
-function lightbox_open(youTubeID) {
-  var lightBoxVideo = document.getElementById("embedded_video");
-  lightBoxVideo.src = 'https://www.youtube.com/embed/'+youTubeID+'?autoplay=1';
-  window.scrollTo(0, 0);
-  document.getElementById('light').style.display = 'block';
-  document.getElementById('fade').style.display = 'block';
-  lightBoxVideo.play();
-}
-
-function lightbox_close() {
-  var lightBoxVideo = document.getElementById("VisaChipCardVideo");
-  document.getElementById('light').style.display = 'none';
-  document.getElementById('fade').style.display = 'none';
-  lightBoxVideo.pause();
+function addToEditor(lines) {
+  const newChunks = lines.split("\n");
+  newChunks.forEach((c) => parley.addChunk(c));
+  parley.run();
 }
---
+++ lessons/hoc-winter-repartee/langs/en-us/pyret.js
@@ -0,0 +1,1 @@
+repartee/pyret.js
