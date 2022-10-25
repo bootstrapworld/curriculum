71b3f9d5839299e179938b9f62b11e6e026bf4bf
Flannery <flannery.denny@gmail.com>
2022-10-25 12:19:42 -0500

    attempt to fix mess I created

---
+++ lessons/hoc-winter-parley/langs/en-us/repartee/precache-manifest.1d4940aaac55c0a1eef2b8ab4a488f0e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d61d743d9d57e76fcb56ae00511c0df",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b51419708821b6b85eda",
+    "url": "./static/css/main.chunk.css"
+  },
+  {
+    "revision": "32e220a3075a79b25bc1",
+    "url": "./static/js/bundle.js"
+  },
+  {
+    "revision": "b51419708821b6b85eda",
+    "url": "./static/js/main.chunk.js"
+  }
+]);
