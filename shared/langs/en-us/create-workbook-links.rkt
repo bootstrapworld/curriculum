1c39b10ec869a2555970f1d9321aa8c3f3996a6a
Dorai Sitaram <ds26gte@gmail.com>
2022-11-07 11:13:58 -0500

    Added Spanish version of create-workbook-links.rkt

--- shared/langs/en-us/create-workbook-links.rkt
+++ shared/langs/en-us/create-workbook-links.rkt
@@ -6,4 +6,4 @@
   (string-append
     "Most exercises are part of the **link:../workbook/workbook.pdf[Student Workbook]**,\n"
     "and we provide password-protected **link:./protected/workbook-sols.pdf.html[Workbook Solutions]** as well.\n\n"
-    "You can find the 'exercise' and 'solution' versions of all supplemental materials as well, in the lists below." ))
+    "You can find the 'exercise' and 'solution' versions of all supplemental materials as well, in the lists below."))
