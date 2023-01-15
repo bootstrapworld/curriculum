e15dfc92071e06e156c797d014d3d0947780d3af
Dorai Sitaram <ds26gte@gmail.com>
2023-01-15 18:15:09 -0500

    Added Makefile which will eventually replace the build-pathway script #467 - Subdir lib/maker contains all the submakefiles and any (small) scripts

---
+++ Makefile
@@ -0,0 +1,20 @@
+# created 2023-01-15
+# last modified 2023-01-15
+
+export NATLANG ?= en-us
+
+export TOPDIR := $(realpath .)
+
+export PROGDIR := $(TOPDIR)/distribution/.prog-$(NATLANG)
+
+export ADOCABLES_INPUT := $(TOPDIR)/distribution/$(NATLANG)/.cached/.do-adocables.rkt
+export ADOC_INPUT := $(TOPDIR)/distribution/$(NATLANG)/.cached/.do-asciidoctor.js
+export ADOC_POSTPROC_PWYINDEP_INPUT := $(TOPDIR)/distribution/$(NATLANG)/.cached/.do-adoc-postproc-pwyindep.txt
+
+export PUPPETEER_INPUT := $(TOPDIR)/distribution/$(NATLANG)/.cached/.do-puppeteer.json
+
+export MAKE_DIR = lib/maker
+
+all:
+	$(MAKE) -I $(MAKE_DIR) -f lib/maker/Makefile.phase1
+	$(MAKE) -I $(MAKE_DIR) -f lib/maker/Makefile.phase2
---
+++ lib/maker/copylessons.mk
@@ -0,0 +1,24 @@
+# created 2023-01-15
+# last modified 2023-01-15
+
+pyret-lessons = $(wildcard lessons/**/langs/$(NATLANG))
+
+pyret-distribution-lessons := $(patsubst %,distribution/$(NATLANG)/lessons/%/., \
+                                $(notdir \
+                                  $(patsubst %/langs/$(NATLANG),%, \
+                                    $(pyret-lessons))))
+
+lesson-bare-name = $(notdir $(patsubst %/langs/$(NATLANG),%,$1))
+
+copylessons: $(pyret-distribution-lessons)
+
+define make-copy-lesson-rule
+distribution/$(NATLANG)/lessons/$(call lesson-bare-name,$1)/.: $1
+	mkdir -p $$@
+	touch $$@
+	cp -upr $$</* $$@
+	$(MAKE_DIR)/massage-distribution-lesson $$@
+
+endef
+
+$(eval $(foreach lesson,$(pyret-lessons),$(call make-copy-lesson-rule,$(lesson))))
