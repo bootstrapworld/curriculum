d2a651a78ebd71f006fb6ec29e16cb9f5c005131
Dorai Sitaram <ds26gte@gmail.com>
2023-01-30 12:36:35 -0500

    Generate google-drive versions of (s)html #467

--- lib/maker/do-postproc.sh
+++ lib/maker/do-postproc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # created 2023-01-19
-# last modified 2023-01-28
+# last modified 2023-01-30
 
 cd $TOPDIR/distribution/$NATLANG
 
@@ -30,13 +30,12 @@ function postproc_func() {
     local localDISTROOTDIR=$(calculatedistrootdir $fhtmlcached)
     local fdir=${fhtmlcached%/.cached/.*html}
     local fbase=${fhtmlcached##*/.}
-    if test -n "$SHTML"; then
-      fbase=${fbase%.html}.shtml
-    fi
+    test -n "$SHTML" && fbase=${fbase%.html}.shtml
     local fhtml=$fdir/$fbase
     cp -p $fhtmlcached $fhtml
     bumpcsspathdir
     resolveabbrevs
+    test -n "$GDRIVE" && insertgdrivesave
   fi
 }
 
@@ -216,11 +215,234 @@ $SED -i \
   fi
 }
 
+function adjustgdrivecolgroup() {
+  local f=$1
+
+  local CTEMP=$(mktemp); rm -f $CTEMP
+
+  local CTEMPFILE=$(basename $CTEMP)-cols.txt
+
+  grep '^<col style.*> *$' $f > $CTEMPFILE
+
+  local num=$(wc -l $CTEMPFILE|$SED -e 's/^ *\([^ ]*\).*/\1/')
+
+  if test $num -eq 0; then
+    rm -f $CTEMPFILE
+    return
+  fi
+
+  $SED -i \
+    -e 's/^<col //' \
+    -e 's/> *$//' \
+    $CTEMPFILE
+
+  local i=1
+
+  local cline
+  local flnum
+
+  while test $i -le $num; do
+    cline=$($SED -ne "$i p" $CTEMPFILE)
+
+    flnum=$(grep -n '^<th ' $f|$SED -e 1q|$SED -e 's/^\([^:]*\).*/\1/')
+
+    $SED -i \
+      -e "$flnum s/^<th /<th%GDRIVE $cline /" \
+      $f
+
+    i=$((i + 1))
+
+  done
+
+  rm -f $CTEMPFILE
+
+  $SED -i \
+    -e 's/^<th%GDRIVE /<th /' \
+    $f
+
+}
+
+function modifygdrivepage() {
+  local f=$1
+  local h=$2
+
+  $SED -i \
+    -e '/include virtual=".menubar.ssi"/d' \
+    -e 's/^<p>Relevant Standards<select.*<\/select><\/p> *$/<p>Standards<\/p>/' \
+    -e '/^<p><em>Select one or more standards from the menu on the left.*<\/p> *$/d' \
+    -e 's/\(class="paragraph \)alignedStandardsIntro/\1/' \
+    -e 's/\(class="dlist \)alignedStandards /\1/' \
+    -e 's/href=\("\(https\|http\):\/\/\)/href%GDRIVEEXTERNALHREF=\1/g' \
+    -e 's/<div/<span/g' \
+    -e 's/<\/div>/<\/span>/g' \
+    -e 's/<dt class="hdlist1">/\0<br\/>/g' \
+    -e 's/<span class="sect1 lesson-section-1">/\0<hr\/>/g' \
+    -e 's/<span class="openblock acknowledgment">/\0<hr\/>/g' \
+    -e 's/<h2.*>\(.*\)<span class="duration">\(.*\)<\/span><\/h2>/<table class="grid-none frame-none section-heading"><tr><td width="65%">\1<\/td><td width="35%">\2<\/td><\/tr><\/table>/g' \
+    -e 's/<tt\(.*\)>\(.*\)<\/tt>/<span\1>\2<\/span>/g' \
+    $f
+  $SED -i \
+    -e 's/^\(.*\)<span\(  *class="paragraph lesson-point"\)/%GDRIVESPANDIV\1<div\2/g' \
+    $f
+  $SED -i \
+    -e '/^%GDRIVESPANDIV/,/^<\/span>/ s/^<\/span>/<\/div>/' \
+    -e 's/^%GDRIVESPANDIV//' \
+    $f
+  $SED -i \
+    -e 's/href="/\0'$hrefPrefix'\//g' \
+    $f
+  $SED -i \
+    -e 's/%GDRIVEEXTERNALHREF//g' \
+    -e '/^ *<link rel="stylesheet".*\.css/d' \
+    -e '/^ *<script .*\(codemirror\|runmode\|scheme2\|pyret-mode\|bootstraplesson\)/d' \
+    -e '/<span class="tooltiptext">.*<\/span>/d' \
+    $f
+  echo "<style>" > $TMPFILE.css
+  #echo doing gdrive-import: PWD is $(pwd)
+  #echo cat $DISTROOTDIR/lib/gdrive-import.css
+  cat lib/gdrive-import.css >> $TMPFILE.css
+  echo "</style>" >> $TMPFILE.css
+  $SED -i \
+    -e '/<\/title>/r '$TMPFILE.css \
+    $f
+
+  $SED -i \
+    -e 's/<span class="begin-ignore-for-gdrive">/<!--\0/g' \
+    -e 's/<span class="end-ignore-for-gdrive"><\/span>/\0-->/g' \
+    $f
+
+  $SED -i \
+    -e '/studentAnswer/ s/&#x5f;/\0\0/g' \
+    $f
+
+  rm -f $TMPFILE.css
+
+  adjustgdrivecolgroup $f
+
+  $SED -i \
+    -e 's/<span class="\(begin\|end\)-ignore-for-gdrive"><\/span>//g' \
+    $h
+
+}
+
+function insertgdrivesave() {
+
+  local f=$fhtml
+  # echo doing insertgdrivesave $f in $(pwd)
+  local f_head=${f%.*}
+  local other_f=$f_head-gdrive-import.html
+  local pageTitle=
+  if test -f $f_head.adoc; then
+    pageTitle=$(grep '^= ' $f_head.adoc|head -n 1|$SED -e 's/^= *//' -e 's/  *$//')
+  fi
+
+  if test ! "$pageTitle"; then
+    pageTitle=$f_head
+  fi
+
+  pageTitle=$(echo $pageTitle|$SED -e 's/[“”]/\\\\"/g')
+  pageTitle=$(echo $pageTitle|$SED -e 's/\//\\\//g')
+
+  local TMPFILE=$f_head.$TMPAFX
+
+  #echo TMPFILE is $TMPFILE
+
+  cat > $TMPFILE-1.txt <<EOF
+  <script>
+  window.status = window.status || 'ready_to_print';
+  window.___gcfg = {
+  parsetags: 'explicit'
+  };
+  </script>
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <script>function renderSaveToDrive() {
+    var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+    gapi.savetodrive.render('savetodrive-div', {
+    src:%INSERTGDRIVEURL,
+    filename:"%INSERTGDRIVELESSONTITLE",
+    sitename:"Bootstrap, Brown University"
+  });
+  if(isSafari){
+    var warning = document.createElement("div");
+    warning.id = "safariWarning";
+    warning.innerHTML = "You appear to be using Safari, which interferes with Google's Save-to-Drive button. You can fix it by going to Preferences, clicking 'Privacy', and making sure that 'Prevent cross-site tracking' is <b>not</b> checked."
+    var button = document.getElementById("savetodrive-div");
+    button.parentNode.insertBefore(warning, warning.nextSibling);
+  }
+}
+</script>
+EOF
+  cat > $TMPFILE-2.txt <<EOF
+  <div id="savetodrive-div"></div>
+
+EOF
+  $SED -i \
+    -e 's/^\(<body  *[^>]*\)>/\1 onload="renderSaveToDrive()">/' \
+    -e 's/<\/head>/%INSERTGDRIVESAVE1\n\0/' \
+    -e 's/<\/h1>.*/\0\n%INSERTGDRIVESAVE2/' \
+    -e '/^<div id="body"/ s/id="body"/\0 onload="renderSaveToDrive()"/' \
+    $f
+
+  if test $WORKBOOKPAGE; then
+    $SED -i \
+      -e 's/^<body class="/\0workbookpage /' \
+      $f
+    if test $NOTESPAGE; then
+      $SED -i \
+        -e 's/^<body class="/\0LessonNotes /' \
+        $f
+    fi
+    if test $BACKMATTERPAGE; then
+      $SED -i \
+        -e 's/^<body class="/\0back-matter /' \
+        $f
+    fi
+  elif test $DATASHEETPAGE; then
+    $SED -i \
+      -e 's/^<body class="/\0datasheetpage /' \
+      $f
+  fi
+
+  if ! grep -q %INSERTGDRIVESAVE2 $f; then
+    $SED -i \
+      -e 's/<div id="header">/\0\n%INSERTGDRIVESAVE2/' \
+      $f
+  fi
+
+  $SED -i \
+    -e '/%INSERTGDRIVESAVE1/r '$TMPFILE-1.txt \
+    -e '/%INSERTGDRIVESAVE2/r '$TMPFILE-2.txt \
+    -e 's/%INSERTGDRIVESAVE[12]//' \
+    $f
+
+  #echo deleting $TMPFILE 12 .txt
+  rm -f $TMPFILE-[12].txt
+
+  $SED -i \
+    -e 's/%INSERTGDRIVELESSONTITLE/'"$pageTitle"'/' \
+    $f
+
+  $CP -p $f $other_f
+
+  $SED -i \
+    -e "s/%INSERTGDRIVEURL/(window.location.href.match(\/\\\\\/$\/)?(window.location.href+'index-gdrive-import.html'):(window.location.href.replace(\/([^\\\\\/]+)\\\\.([^.\\\\\/]+)$\/, '\$1-gdrive-import.html')))/" \
+    $f
+
+  $SED -i \
+    -e 's/%INSERTGDRIVEURL/window.location.href/' \
+    $other_f
+
+  modifygdrivepage $other_f $f
+}
+
+#############################################################################
+
+GDRIVE=1
+
 run_postproc postproc_pwyindep $ADOC_POSTPROC_PWYINDEP_INPUT
 run_postproc postproc_workbookpage $ADOC_POSTPROC_WORKBOOKPAGE_INPUT
 
-export SHTML=1
-run_postproc postproc_lessonplan $ADOC_POSTPROC_LESSONPLAN_INPUT
-run_postproc postproc_narrative $ADOC_POSTPROC_NARRATIVE_INPUT
-run_postproc postproc_narrativeaux $ADOC_POSTPROC_NARRATIVEAUX_INPUT
-run_postproc postproc_resources $ADOC_POSTPROC_RESOURCES_INPUT
+SHTML=1 run_postproc postproc_lessonplan $ADOC_POSTPROC_LESSONPLAN_INPUT
+SHTML=1 run_postproc postproc_narrative $ADOC_POSTPROC_NARRATIVE_INPUT
+SHTML=1 GDRIVE= run_postproc postproc_narrativeaux $ADOC_POSTPROC_NARRATIVEAUX_INPUT
+SHTML=1 run_postproc postproc_resources $ADOC_POSTPROC_RESOURCES_INPUT
