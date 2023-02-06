14a0ea3d3d922904f89ad47338991da4f86ef81a
Dorai Sitaram <ds26gte@gmail.com>
2023-02-06 15:51:42 -0500

    make: add rule for link validity check

--- lib/maker/do-link-check.sh
+++ lib/maker/do-link-check.sh
@@ -1,5 +1,7 @@
 # created 2023-01-20
-# last modified 2023-01-27
+# last modified 2023-02-06
+
+cd $TOPDIR/distribution/$NATLANG
 
 find . -type f -name \.\*external-links.txt.kp | xargs $PROGDIR/checkexternallinks
 find . -type f -name \.\*internal-links.txt.kp | xargs $PROGDIR/checkinternallinks
