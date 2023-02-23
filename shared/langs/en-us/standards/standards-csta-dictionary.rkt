bootstrapworld/curriculum

commit cafa7c2ae038556b3d215cca799a23826860c13b

update CSTA standards to include early-math

shared/langs/en-us/standards/standards-csta-dictionary.rkt
@@ -87,7 +87,7 @@
      "Use data analysis tools and techniques to identify patterns in data representing complex systems"
      "scatter-plots"
      "linear-regression"
-     "advanced-displays"
+     "custom-scatter-plots"
      "correlations"
      )
     ("3B-NI-06"
@@ -131,7 +131,7 @@
      "project-exploration-paper"
      "table-methods"
      "probability-inference"
-     "advanced-displays"
+     "custom-scatter-plots"
      "grouped-samples"
      "bar-and-pie-charts"
      )
@@ -175,6 +175,12 @@
     ;;;;;;;;;;;;;; Algorithms and Programming ;;;;;;;;;;;;;;;;;;;;;;
     ("1B-AP-08"
      "Compare and refine multiple algorithms for the same task and determine which is the most appropriate."
+     "order-of-operations"
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
      )
     ("1B-AP-09"
      "Create programs that use variables to store and modify data."
@@ -187,6 +193,7 @@
      "project-exploration-paper"
      "project-video-game"
      "defining-values"
+     "equivalence"
      )
     ("1B-AP-10"
      "Create programs that include sequences, events, loops, and conditionals."
@@ -196,7 +203,7 @@
      "project-research-paper"
      "project-exploration-paper"
      "composing-table-operations"
-     "advanced-displays"
+     "custom-scatter-plots"
      "project-video-game"
      "player-animation"
      "functions-for-character-animation"
@@ -211,6 +218,16 @@
      "choosing-your-dataset"
      "project-video-game"
      "problem-decomposition"
+     "order-of-operations"
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
+     "absolute-value-and-opposites"
+     "absolute-value-and-opposites-2"
+     "evaluating-exponents"
+     "exponent-expressions-with-negatives"
      )
     ("1B-AP-12"
      "Modify, remix, or incorporate portions of an existing program into one's own work, to develop something new or add more advanced features."
@@ -221,7 +238,7 @@
      "re-intro-to-data-structures"
      "project-research-paper"
      "project-exploration-paper"
-     "advanced-displays"
+     "custom-scatter-plots"
      "project-video-game"
      "player-animation"
      "making-game-images"
@@ -248,6 +265,15 @@
      "project-video-game"
      "debugging"
      "functions-make-life-easier"
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
+     "absolute-value-and-opposites"
+     "absolute-value-and-opposites-2"
+     "evaluating-exponents"
+     "exponent-expressions-with-negatives"
      )
     ("1B-AP-16"
      "Take on varying roles, with teacher guidance, when collaborating with peers during the design, implementation, and review stages of program development."
@@ -261,18 +287,38 @@
      "re-intro-to-data-structures"
      "project-research-paper"
      "project-exploration-paper"
+     "order-of-operations"
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
+     "absolute-value-and-opposites"
+     "absolute-value-and-opposites-2"
+     "evaluating-exponents"
+     "exponent-expressions-with-negatives"
      )
     ("2-AP-10"
      "Use flowcharts and/or pseudocode to address complex problems as algorithms"
      "re-structures-reactors-animations"
-     )
+     "order-of-operations"
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
+     "absolute-value-and-opposites"
+     "absolute-value-and-opposites-2"
+     "evaluating-exponents"
+     "exponent-expressions-with-negatives"
+      )
     ("2-AP-11"
      "Create clearly named variables that represent different data types and perform operations on their values."
      "re-structures-reactors-animations"
      "re-intro-to-data-structures"
      "project-research-paper"
      "project-exploration-paper"
-     "advanced-displays"
+     "custom-scatter-plots"
      "grouped-samples"
      "project-video-game"
      "surface-area-rect-prism"
@@ -306,7 +352,17 @@
      "defining-table-functions"
      "project-video-game"
      "problem-decomposition"
-     )
+     "order-of-operations"
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
+     "absolute-value-and-opposites"
+     "absolute-value-and-opposites-2"
+     "evaluating-exponents"
+     "exponent-expressions-with-negatives"
+          )
     ("2-AP-14"
      "Create procedures with parameters to organize code and make it easier to reuse."
      "re-scoring"
@@ -341,7 +397,7 @@
      "project-research-paper"
      "project-exploration-paper"
      "composing-table-operations"
-     "advanced-displays"
+     "custom-scatter-plots"
      "defining-table-functions"
      "checking-your-work"
      "project-video-game"
@@ -362,7 +418,7 @@
      "Document programs in order to make them easier to follow, test, and debug."
      "project-research-paper"
      "project-exploration-paper"
-     "advanced-displays"
+     "custom-scatter-plots"
      "project-video-game"
      "surface-area-rect-prism"
      "problem-decomposition"
@@ -461,7 +517,16 @@
      )
     ("3B-AP-11"
      "Evaluate algorithms in terms of their efficiency, correctness, and clarity."
-     )
+     "commutative-property"
+     "associative-property"
+     "equivalence"
+     "identity-property"
+     "distributive-property"
+     "absolute-value-and-opposites"
+     "absolute-value-and-opposites-2"
+     "evaluating-exponents"
+     "exponent-expressions-with-negatives"   
+       )
     ("3B-AP-12"
      "Compare and contrast fundamental data structures and their uses."
      )
@@ -476,7 +541,7 @@
      "re-adding-collisions"
      "project-research-paper"
      "project-exploration-paper"
-     "advanced-displays"
+     "custom-scatter-plots"
      "histograms2"
      "histograms"
      "choosing-your-dataset"
@@ -628,6 +693,7 @@
      )
     ("3B-IC-28"
      "Debate laws and regulations that impact the development and use of software."
+     "ethics-privacy-and-bias"
      )
     ))
 

commit 1650f69d3e9dd3ea89c35f1587326b8e9e6f2bc3

fix alignment bugs introduced in cafa7c2ae038556b3d215cca799a23826860c13b

shared/langs/en-us/standards/standards-csta-dictionary.rkt
@@ -87,7 +87,7 @@
      "Use data analysis tools and techniques to identify patterns in data representing complex systems"
      "scatter-plots"
      "linear-regression"
-     "custom-scatter-plots"
+     "advanced-displays"
      "correlations"
      )
     ("3B-NI-06"
@@ -131,7 +131,7 @@
      "project-exploration-paper"
      "table-methods"
      "probability-inference"
-     "custom-scatter-plots"
+     "advanced-displays"
      "grouped-samples"
      "bar-and-pie-charts"
      )
@@ -203,7 +203,7 @@
      "project-research-paper"
      "project-exploration-paper"
      "composing-table-operations"
-     "custom-scatter-plots"
+     "advanced-displays"
      "project-video-game"
      "player-animation"
      "functions-for-character-animation"
@@ -238,7 +238,7 @@
      "re-intro-to-data-structures"
      "project-research-paper"
      "project-exploration-paper"
-     "custom-scatter-plots"
+     "advanced-displays"
      "project-video-game"
      "player-animation"
      "making-game-images"
@@ -318,7 +318,7 @@
      "re-intro-to-data-structures"
      "project-research-paper"
      "project-exploration-paper"
-     "custom-scatter-plots"
+     "advanced-displays"
      "grouped-samples"
      "project-video-game"
      "surface-area-rect-prism"
@@ -397,7 +397,7 @@
      "project-research-paper"
      "project-exploration-paper"
      "composing-table-operations"
-     "custom-scatter-plots"
+     "advanced-displays"
      "defining-table-functions"
      "checking-your-work"
      "project-video-game"
@@ -418,7 +418,7 @@
      "Document programs in order to make them easier to follow, test, and debug."
      "project-research-paper"
      "project-exploration-paper"
-     "custom-scatter-plots"
+     "advanced-displays"
      "project-video-game"
      "surface-area-rect-prism"
      "problem-decomposition"
@@ -541,7 +541,7 @@
      "re-adding-collisions"
      "project-research-paper"
      "project-exploration-paper"
-     "custom-scatter-plots"
+     "advanced-displays"
      "histograms2"
      "histograms"
      "choosing-your-dataset"