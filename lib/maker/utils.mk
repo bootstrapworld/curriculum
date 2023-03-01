294c27ca3d465ecdb87da8cecf926bb5612fa713
Dorai Sitaram <ds26gte@gmail.com>
2023-03-01 18:58:52 -0500

    reduce population of distribution/.prog-en-us

--- lib/maker/utils.mk
+++ lib/maker/utils.mk
@@ -1,6 +1,9 @@
-# created 2023-01-17
-# last modified 2023-02-13
+# last modified 2023-03-01
+
+# asc-file of wherever/it/is/file.adoc is wherever/it/is/.cached/.file.asc
 
 asc-file = $(dir $1).cached/.$(patsubst %.adoc,%.asc,$(notdir $1))
+
+# pdf-file of wherever/it/is/file.adoc is wherever/it/is/file.pdf
 
 pdf-file = $(dir $1)$(patsubst %.adoc,%.pdf,$(notdir $1))
