1d2f66bf16c902620b62b11619c326e5b66049a8
Dorai Sitaram <ds26gte@gmail.com>
2023-03-07 14:29:05 -0500

    dir-hoist-timestamp.sh: Extend macOS special treatment to WSL Ubuntu too

--- lib/maker/dir-hoist-timestamp.sh
+++ lib/maker/dir-hoist-timestamp.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# last modified 2023-03-06
+# last modified 2023-03-07
 
-running_macOS=
+running_problematic_OS=
 
-(echo $OSTYPE | grep -q darwin) && running_macOS=yes
+(echo $OSTYPE | grep -q darwin) && running_problematic_OS=yes
+
+test "$WSL_DISTRO_NAME" = Ubuntu && running_problematic_OS=yes
 
 function hoist() {
   local d=$1
@@ -14,7 +16,7 @@ function hoist() {
     if test -d "$dd"; then
       hoist "$dd"
       test "$dd" -nt "$d" && touch -r "$dd" "$d"
-    elif test -f "$dd" -a -n "$running_macOS"; then
+    elif test -f "$dd" -a -n "$running_problematic_OS"; then
       local d_t=$(date +%s -r "$d")
       local dd_t=$(date +%s -r "$dd")
       test $dd_t -gt $d_t && touch -r "$dd" "$d"
