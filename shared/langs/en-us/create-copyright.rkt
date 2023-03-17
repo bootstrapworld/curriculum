9c1f61527dbeace5fe0b3fd25824e0a524b0b3cd
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2023-03-17 16:06:29 -0400

    Dynamically-compute starter files for a pathway (#1297)
    This commit adds starter files to the dependency graph, so that a pathway's Teacher Resource page can list all the starter files automatically rather than relying on authors to hard-code the list. This removes another likely source of bugs, and also allows for user-created pathways to have a more useful teacher resource page.
    
    Note: also adds starter-files.js, which declares a global JSON dictionary of starter files.
    
    Closes #1283

--- shared/langs/en-us/create-copyright.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(provide create-copyright)
-
-(define (create-copyright name)
-  (format "~a by the https://bootstrapworld.org/community/index.shtml[Bootstrap Community] is licensed under a https://creativecommons.org/licenses/by-nc-nd/4.0/[Creative Commons 4.0 Unported License]. This license does not grant permission to run training or professional development. Offering training or professional development with materials substantially derived from Bootstrap must be approved in writing by a Bootstrap Director. Permissions beyond the scope of this license, such as to run training, may be available by contacting mailto:contact@BootstrapWorld.org[contact@BootstrapWorld.org]. "
-          name
-          ))
