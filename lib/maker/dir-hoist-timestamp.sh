32dc5d47e661d0108c5178ffaa234b1987a6b247
Dorai Sitaram <ds26gte@gmail.com>
2023-03-27 08:12:20 -0400

    Revert "dir-hoist-timestamp.sh: Use gdate instead of date on macOS"
    This reverts commit e36e93829fd9321224dd0a522d2212e9ade2e64b.

--- lib/maker/dir-hoist-timestamp.sh
+++ lib/maker/dir-hoist-timestamp.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-# last modified 2023-03-26
+# last modified 2023-03-07
 
 running_problematic_OS=
 
 (echo $OSTYPE | grep -q darwin) && running_problematic_OS=yes
 
 test "$WSL_DISTRO_NAME" = Ubuntu && running_problematic_OS=yes
-
-DATE=date
-
-if test -n "$running_problematic_OS"; then
-  if which gdate|grep -q .; then
-    DATE=gdate
-  fi
-fi
 
 function hoist() {
   local d=$1
@@ -25,8 +17,8 @@ function hoist() {
       hoist "$dd"
       test "$dd" -nt "$d" && touch -r "$dd" "$d"
     elif test -f "$dd" -a -n "$running_problematic_OS"; then
-      local d_t=$($DATE +%s -r "$d")
-      local dd_t=$($DATE +%s -r "$dd")
+      local d_t=$(date +%s -r "$d")
+      local dd_t=$(date +%s -r "$dd")
       test $dd_t -gt $d_t && touch -r "$dd" "$d"
     fi
   done
