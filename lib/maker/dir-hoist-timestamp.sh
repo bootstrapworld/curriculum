2605ba954ae67990e3e4aea2760e71c9d435892c
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2023-04-27 14:23:35 -0400

    much faster method of finding most-recently modified file

--- lib/maker/dir-hoist-timestamp.sh
+++ lib/maker/dir-hoist-timestamp.sh
@@ -8,7 +8,7 @@ function updateLangTimestamp() {
     # if the parent is a langs directory, it's the language folder for a lesson
     # set the modification date to be that of the most-recently modified file
     if [[ $d = *langs ]]; then
-      touch -mr $(find $dd -type f -exec stat -c "%Y %n" {} \; | sort -nr | head -1 | cut -d' ' -f2-) $dd
+      touch -mr $(find $dd -type f -print0 | xargs -0 stat --format '%Y %n' | sort -nr | cut -d' ' -f2- | head -1) $dd
     # if it's a directory that is NOT a lesson, walk through it looking for lessons
     elif test -d "$dd"; then
       updateLangTimestamp "$dd"
@@ -17,7 +17,7 @@ function updateLangTimestamp() {
 }
 
 for dir in "$@"; do
-  test -d "$dir" && updateLangTimestamp "$dir"
+  test -d "$dir" && $(time updateLangTimestamp "$dir")
 done
 
 exit 0
