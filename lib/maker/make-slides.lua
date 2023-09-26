eed353c51f8ace25e9e51b522a06ba2a0a331f92
Dorai Sitaram <ds26gte@gmail.com>
2023-09-26 11:54:11 -0400

    make-slides.lua: Rewrite to collect slide segments first to allow better processing #1506

--- lib/maker/make-slides.lua
+++ lib/maker/make-slides.lua
@@ -13,55 +13,152 @@ function first_line(f)
   return x
 end
 
-local course_string = first_line('.cached/.record-superdir') or 'Default'
+local lesson_superdir = first_line('.cached/.record-superdir') or 'Default'
 
-function make_slides_file()
+local course_string = 'Default'
+
+if lesson_superdir == 'Data-Science' or lesson_superdir == 'Algebra2' then
+  course_string = 'DS'
+elseif lesson_superdir == 'Algebra' or lesson_superdir == 'Early-Math' then
+  course_string = 'A'
+end
+
+function get_segments(lplan_file)
   local i = io.open(lplan_file)
-  if not i then return end
+  local segments = {}
+  local current_segment = false
+  --
+  for L in i:lines() do
+    if L:match('^=') or L:match('^%s*@slidebreak') then
+      if current_segment then
+        segments[#segments + 1] = current_segment
+      end
+      current_segment = {}
+    end
+    if current_segment then
+      current_segment[#current_segment + 1] = L
+    end
+  end
+  --
+  i:close()
+  --
+  return segments
+end
+
+function type_of_slide(segment)
+  local L1 = segment[1]
+  if not L1 then
+    return 2
+  elseif L1:match('^= ') then
+    return 0
+  elseif L1:match('^== ') then
+    return 1
+  else
+    return 2
+  end
+end
+
+function find_segment_title(segment)
+  local L = segment[1]
+  if not L then
+    return nil
+  elseif L:match('^=') then
+    local heading = L:gsub('^=+%s*(.*)', '%1')
+    heading = heading:gsub('@duration.*', '')
+    return heading
+  else
+    return nil
+  end
+end
+
+function number_of_images(segment)
+  local n = 0
+  for _,L in ipairs(segment) do
+    if L:match('@image') then
+      n = n + 1
+    end
+  end
+  return n
+end
+
+function find_section(segment)
+  return 'Launch'
+end
+
+function contains_center(segment)
+  for _,L in ipairs(segment) do
+    if L:match('@center') then
+      return true
+    end
+  end
+  return false
+end
+
+function find_image_orientation(segment, default)
+  default = default or 'R'
+  local centered = contains_center(segment)
+  if centered then
+    return 'C'
+  else
+    local n = number_of_images(segment)
+    if n == 1 then
+      return 'R'
+    else
+      return default
+    end
+  end
+end
+
+function find_suffix(segment)
+  for _,L in ipairs(segment) do
+    if L:match('%[%.lesson%-instruction%]') then
+      return '-DN'
+    end
+  end
+  return ''
+end
+
+function make_slides_file(lplan_file, slides_file)
+
+  local segments = get_segments(lplan_file)
+
+  local segments_last_index = #segments
 
   local o = io.open(slides_file, 'w')
 
-  local current_slide_title = 'default slide title'
+  local current_title = 'not_yet_set'
 
-  local processing_slides = false
-
-  local section_type = 'Launch'
-
-  for L in i:lines() do
-    if (not processing_slides) and L:match('^= ') then
-      local title_string = L:gsub('^=%s*(.*)', '%1')
-      current_slide_title = title_string
+  for k,current_segment in ipairs(segments) do
+    local section
+    if k == segments_last_index then
+      section = 'Supplemental'
+    else
+      section = find_section(current_segment)
+    end
+    local image_orientation = find_image_orientation(current_segment)
+    local suffix = find_suffix(current_segment)
+    local level = type_of_slide(current_segment)
+    if level <= 1 then
+      current_title = find_segment_title(current_segment)
+    end
+    if k == 1 then
       o:write('---\n')
       o:write('{layout="', course_string, ' Title Slide"}\n')
-      o:write('# ', title_string, '\n\n')
+      o:write('# ', current_title, '\n\n')
       o:write('<!--\n')
       o:write('To learn more about how to use PearDeck, and how to view the embedded links on these slides without going into present mode visit https://help.peardeck.com/en\n')
       o:write('-->\n')
-    end
-    if L:match('^== ') or L:match('^=== ') or L:match('^%s*@slidebreak') then
-      if L:match('^== ') then
-        processing_slides = false
-        current_slide_title = L:gsub('^==%s*(.*)', '%1')
-        current_slide_title = current_slide_title:gsub('@duration.*', '')
-        -- section_type is one of: Launch, Investigate, Synthesize
-        -- needs to be fleshed out
-        if current_slide_title:match('[Ll]aunch') then
-          section_type = 'Launch'
-        else
-          section_type = 'Investigate'
+    elseif level ~= 1 then
+      o:write('---\n')
+      o:write('{layout="', section, image_orientation, suffix, '"}\n')
+      o:write('# ', current_title, '\n\n')
+      for j,L in ipairs(current_segment) do
+        if j ~= 1 then
+          o:write(L, '\n')
         end
-      else
-        processing_slides = true
-        o:write('---\n')
-        o:write('{layout="', section_type, '"}\n')
-        o:write('# ', current_slide_title, '\n\n')
       end
-    elseif processing_slides then
-      o:write(L, '\n')
     end
   end
-
-  o:close()
 end
 
-make_slides_file()
+make_slides_file(lplan_file, slides_file)
