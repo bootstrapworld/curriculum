b306c73c98139466465d2e96493306409c6f1bf2
Dorai Sitaram <ds26gte@gmail.com>
2023-10-11 09:39:30 -0400

    - Added lang-specific defines.lua (a la defines.rkt) to specify snippets to be inserted by Lua programs - Specifically, define glossary_preface to add feedback-requesting prose   to bilingual glossary #1560

---
+++ shared/langs/en-us/defines.lua
@@ -0,0 +1,6 @@
+glossary_preface = [[
+Thanks to everyone whose help has allowed us to offer this bilingual glossary!
+
+If you have suggestions on how to improve any of our Spanish definitions,
+please share your comments with contact@BootstrapWorld.org
+]]
