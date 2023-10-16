18717f7a4e3816646e2be161b6fea2f2be13d45f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2023-10-16 10:56:33 -0400

    remove watch-author, as it is no longer needed

--- watch-author.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
- 
-# Keep an eye on lessons/, pathways/, shared/, and lib/shared.less
-# Rebuild if a file ending in adoc, less, css, md, or txt is modified
-fswatch -or \
-    ./lessons ./pathways ./shared ./lib/shared.less \
-    -e ".*" \
-    -i "\\.adoc$" -i "\\.less$" -i "\\.css$" -i "\\.md$" -i "\\.txt$" -i "\\.shtml$" \
-    | xargs -n1 -I{} sh -c "make -j 32 && echo Auto-Rebuild Complete!"
