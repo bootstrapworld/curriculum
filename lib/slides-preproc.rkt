d995a13ae5f182755d2132e46d1a6aed21d09f6a
Dorai Sitaram <ds26gte@gmail.com>
2023-11-07 12:25:38 -0500

    slide building should keep track of natlang

--- lib/slides-preproc.rkt
+++ lib/slides-preproc.rkt
@@ -26,7 +26,7 @@
 
 (define *proglang-sym* (string->symbol *proglang*))
 
-(define *natlang* 'en-us)
+(define *natlang* (or (getenv "NATLANG") "en-us"))
 
 (define *starter-files*
   (let ([starter-files-file (format "~a/distribution/~a/starter-files.js"
