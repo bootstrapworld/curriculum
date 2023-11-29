473bc29d8b544371812fdc95f277abfa3734b900
Dorai Sitaram <ds26gte@gmail.com>
2023-11-29 05:25:01 -0500

    DEBUGADOC=t: use red message, show progress so usr knows something's happening

--- lib/maker/run-asciidoctor.sh
+++ lib/maker/run-asciidoctor.sh
@@ -2,7 +2,7 @@
 
 $PROGDIR/adocables-preproc.rkt $ADOCABLES_INPUT
 
-test -f "$ADOC_INPUT" || exit 0
+test -s "$ADOC_INPUT" || exit 0
 
 # echo calling asciidoctor in dir $(pwd)
 
@@ -10,17 +10,20 @@ if test -z "$ASCIIDOCTOR_NODE"; then
   if test -z "$DEBUGADOC"; then
     asciidoctor -a linkcss -a stylesheet=$TOPDIR/distribution/$NATLANG/lib/curriculum.css -a cachedir=.cached/ -B . $(cat $ADOC_INPUT)
   else
-    echo 🐌 Will be slow! Running asciidoctor once per file because DEBUGADOC = $DEBUGADOC
+    echo $'\e[1;31m'🐌 Will be slow! Running asciidoctor once per file because DEBUGADOC=$DEBUGADOC $'\e[0m'
     errfile=.error.tmp
     for f in $(cat $ADOC_INPUT); do
       rm -f $errfile; touch $errfile
       # echo asciidoctor $f
+      echo -n .
       asciidoctor -a linkcss -a stylesheet=$TOPDIR/distribution/$NATLANG/lib/curriculum.css -a cachedir=.cached/ -B . $f > $errfile 2>&1
       if test -s "$errfile"; then
+        echo
         echo Error occurred while asciidoctoring $f
         cat $errfile
       fi
     done
+    echo
   fi
 else
   NODE_ADOC_INPUT=$ADOC_INPUT-node.js
