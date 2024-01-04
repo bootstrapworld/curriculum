ed05869c9268e2fa6f25234c0b9df2c8bd9ffbeb
Dorai Sitaram <ds26gte@gmail.com>
2024-01-03 21:12:51 -0500

    make-wp-csv.rkt: Create 3 sepearate csv's

--- lib/make-wp-csv.rkt
+++ lib/make-wp-csv.rkt
@@ -13,7 +13,9 @@
 (define *lessons-dir* (format "~a/lessons" *dist-en-us*))
 (define *courses-dir* (format "~a/courses" *dist-en-us*))
 
-(define *csv-file* "wp.csv")
+(define *lesson-csv-file* "lessons.csv")
+(define *page-csv-file* "xyz-pages.csv")
+(define *solution-page-csv-file* "xyz-solution-pages.csv")
 
 (define (string->uniqid s)
   (when (path? s)
@@ -59,30 +61,34 @@
                  (string-titlecase *season*)
                  *year*)))))
 
-(define (convert-workbook-pages o)
+(define (convert-workbook-pages o #:pages [pages "pages"])
+  ; (printf "doing convert-workbook-pages ~a\n" pages)
   (for ([lesson-dir (directory-list *lessons-dir*)])
     (let* ([lesson-dir-path (build-path *lessons-dir* lesson-dir)]
            [lesson-plan-file (path->string (build-path lesson-dir-path "index.shtml"))]
            [pages-dir (build-path lesson-dir "pages")]
            [solution-pages-dir (build-path lesson-dir "solution-pages")])
-      (for ([pages '("pages" "solution-pages")])
-        (let ([pages-dir-path (build-path lesson-dir-path pages)])
-          (when (directory-exists? pages-dir-path)
-            (for ([p (directory-list pages-dir-path)])
-              (set! p (path->string p))
-              (when (regexp-match #rx"\\.html" p)
-                ;id, title, permalink, lesson-parent, unit-raw-code
-                (let* ([q-p (build-path pages p)]
-                       [page-file (path->string (build-path pages-dir-path p))]
-                       [page-title-file (path->string (build-path pages-dir-path ".cached"
-                                                                  (string-append "." p ".titletxt")))])
-                  (when (file-exists? page-title-file)
-                    (fprintf o "~a,\"~a\",~a,\"a:1:{i:0;s:2:\"\"~a\"\";}\",\"~a\"\n"
-                             (string->uniqid q-p)
-                             (string-trim (escaped-file-content page-title-file #:kill-newlines? #t))
-                             (make-page-permalink q-p)
-                             (string->uniqid lesson-plan-file)
-                             (escaped-file-content page-file))))))))))))
+      (let ([pages-dir-path (build-path lesson-dir-path pages)])
+        (when (directory-exists? pages-dir-path)
+          (for ([p (directory-list pages-dir-path)])
+            ; (printf "doing p = ~a\n" p)
+            (set! p (path->string p))
+            (when (regexp-match #rx"\\.html" p)
+              ;id, title, permalink, lesson-parent, unit-raw-code
+              (let* ([q-p (build-path pages p)]
+                     [page-file (path->string (build-path pages-dir-path p))]
+                     [page-title-file (path->string (build-path pages-dir-path ".cached"
+                                                                (string-append "."
+                                                                  (regexp-replace #rx"\\.html" p ".titletxt"))))])
+                ; (printf "page-title-file is ~a\n" page-title-file)
+                (when (file-exists? page-title-file)
+                  ; (printf "going ahead with ~a\n" p)
+                  (fprintf o "~a,\"~a\",~a,\"a:1:{i:0;s:2:\"\"~a\"\";}\",\"~a\"\n"
+                           (string->uniqid q-p)
+                           (string-trim (escaped-file-content page-title-file #:kill-newlines? #t))
+                           (make-page-permalink q-p)
+                           (string->uniqid lesson-plan-file)
+                           (escaped-file-content page-file)))))))))))
 
 (let* ([cla (current-command-line-arguments)]
        [n (vector-length cla)])
@@ -93,9 +99,13 @@
 
 ; (printf "season = ~s; year = ~s\n" *season* *year*)
 
-(call-with-output-file *csv-file*
-  (lambda (o)
-    (convert-lessons o)
-    (convert-workbook-pages o))
+(call-with-output-file *lesson-csv-file* convert-lessons
+  #:exists 'replace)
+
+(call-with-output-file *page-csv-file* convert-workbook-pages
+  #:exists 'replace)
+
+(call-with-output-file *solution-page-csv-file*
+  (lambda (o) (convert-workbook-pages o #:pages "solution-pages"))
   #:exists 'replace)
 
