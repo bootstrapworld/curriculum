5e147da713f711c5a86d9e7abefba9dd8c5e68a8
retabak <rachel@bootstrapworld.org>
2024-01-22 11:08:37 -0500

    remove string in url

--- shared/langs/en-us/starter-files.js
+++ shared/langs/en-us/starter-files.js
@@ -699,7 +699,7 @@ const EarlyMath = {
   "identity-prop":{
     "title": "Identity Property Starter File",
     "pyret": {
-      "url": "https://code.pyret.org/editor#share=1pedISvOwuhhPfPD_vOTwrTEY--IcJnWm&v=78aeaeb"
+      "url": "https://code.pyret.org/editor#share=1pedISvOwuhhPfPD_vOTwrTEY--IcJnWm"
     },
     "wescheme": {
       "url": "https://www.wescheme.org/openEditor?publicId=5hRzeYdvpB"
