a8e233afcdbd6baec6f24401a806beafd386e598
flannery-denny <flannery@bootstrapworld.org>
2024-01-24 14:00:28 -0600

    contracts update (see #1669)

--- shared/langs/en-us/starter-files.js
+++ shared/langs/en-us/starter-files.js
@@ -471,6 +471,18 @@ const DSdesmos = {
   }
 }
 
+const Desmos = {
+  "images-code":{
+    "title": "Matching Images to Code (Desmos)",
+    "pyret":{
+      "url": "https://teacher.desmos.com/activitybuilder/custom/5fc90a1937b6430d836b67e7"
+    },
+    "wescheme":{
+      "url": "https://teacher.desmos.com/activitybuilder/custom/5fecf203a29e040d182be6c6"
+    }
+  }
+}
+
 const Flags = {
   "flags": {
     "title": "Flags Starter File",
@@ -930,6 +942,7 @@ var allStarterFiles = {
   ...MoreDS, 
   ...DSadvanceDisplays,
   ...DSdesmos,
+  ...Desmos,
   ...MoreAlg,
   ...EarlyMath,
   ...Projects, 
