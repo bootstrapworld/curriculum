69b5cd7719ded56b7e6dad37ad965514ba2f46b0
Dorai Sitaram <ds26gte@gmail.com>
2024-01-30 09:30:30 -0500

    Bring back `Last updated` timestamp, but to minimize pointless diffs, use the timestamp of the last git commit. If not inside a git repo, use the timestamp of the last midnight

--- lib/maker/run-asciidoctor.sh
+++ lib/maker/run-asciidoctor.sh
@@ -12,16 +12,22 @@ errfile=.make.error.log
 
 rm -f $errfile
 
+if git rev-parse --show-toplevel >/dev/null 2>/dev/null; then
+  export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+else
+  export SOURCE_DATE_EPOCH=$(date -d $(date +%Y-%m-%d) +%s)
+fi
+
 if test -z "$ASCIIDOCTOR_NODE"; then
   if test -z "$DEBUGADOC"; then
-    asciidoctor -a linkcss -a reproducible -a stylesheet=$cssfile -a cachedir=.cached/ -B . $(cat $ADOC_INPUT) > $errfile 2>&1
+    asciidoctor -a linkcss -a stylesheet=$cssfile -a cachedir=.cached/ -B . $(cat $ADOC_INPUT) > $errfile 2>&1
   else
     echo $'\e[1;31m'🐌 Will be slow! Running asciidoctor once per file because DEBUGADOC=$DEBUGADOC $'\e[0m'
     for f in $(cat $ADOC_INPUT); do
       rm -f $errfile; touch $errfile
       # echo asciidoctor $f
       echo -n .
-      asciidoctor -a linkcss -a reproducible -a stylesheet=$cssfile -a cachedir=.cached/ -B . $f > $errfile 2>&1
+      asciidoctor -a linkcss -a stylesheet=$cssfile -a cachedir=.cached/ -B . $f > $errfile 2>&1
       if test -s "$errfile"; then
         echo
         echo Error occurred while asciidoctoring $f
