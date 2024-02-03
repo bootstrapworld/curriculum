6db1b3dc0cace105589b4f557e7e63ba69dff59c
retabak <rachel@bootstrapworld.org>
2024-02-02 20:55:57 -0500

    respond to ES feedback

--- shared/langs/en-us/starter-files.js
+++ shared/langs/en-us/starter-files.js
@@ -723,7 +723,7 @@ const EarlyMath = {
   "negation":{
     "title": "Negation Starter File",
     "pyret": {
-      "url": "https://code.pyret.org/editor#share=1JJcf8ixS0H3xLWYAd-dSbZDk8bs-IQvy&v=0c33ff0"
+      "url": "https://code.pyret.org/editor#share=1wx5GeCHoysnqsc9G9x3Eb5IAYRK5eoMJ&v=0c33ff0"
     },
     "wescheme": {
       "url": "https://www.wescheme.org/view?publicId=TVUznVqHyA"
