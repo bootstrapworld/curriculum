5845bd4312d0290f643b0aa1da5c0035550c09d9
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2024-02-06 22:43:27 -0500

    [lib] remove errant debugging statement from preproc.rkt

--- lib/preproc.rkt
+++ lib/preproc.rkt
@@ -1225,9 +1225,7 @@
         ;(printf "lesson-description is ~s\n" lesson-description)
         ;lesson-index-file (shtml) doesn't exist yet, but shd we at least check for index.adoc?
         (if (string-prefix? lesson "project-")
-          (begin 
-            (printf "~a found a project!\n" lesson)
-            (fprintf o "link:pass:[~a~a?pathway=~a][~a, role=project] ::" *dist-root-dir* lesson-index-file *target-pathway* lesson-title))
+          (fprintf o "link:pass:[~a~a?pathway=~a][~a, role=project] ::" *dist-root-dir* lesson-index-file *target-pathway* lesson-title)
           (fprintf o "link:pass:[~a~a?pathway=~a][~a] ::" *dist-root-dir* lesson-index-file *target-pathway* lesson-title)
           )
         
