8b19dd250f01e4db3f9cea20d36d52a24089c833
Dorai Sitaram <ds26gte@gmail.com>
2024-02-19 12:05:55 -0500

    make-slides.lua: convert ---- to Markdown code fences #1858

--- lib/maker/make-slides.lua
+++ lib/maker/make-slides.lua
@@ -302,6 +302,7 @@ local function make_slides_file(lplan_file, slides_file)
         local slide_lines = string_split(slide.text, '\n')
         for _,l1 in ipairs(slide_lines) do
           l1 = l1:gsub('^(%s+)#', '%1\\#')
+          l1 = l1:gsub('^%-%-%-%-$', '````')
           l1 = l1:gsub('__(.-)__', '_%1_')
           l1 = l1:gsub('%*%*(.-)%*%*', '*%1*')
           l1 = l1:gsub('%*(%S.-%S)%*', '__%1__')
