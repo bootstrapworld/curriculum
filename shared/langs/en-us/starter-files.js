723dd0471e51e7358e08d5930fa59c7f26e04e23
retabak <rachel@bootstrapworld.org>
2024-04-03 12:51:00 -0400

    negate starter files for wescheme (#1978)

--- shared/langs/en-us/starter-files.js
+++ shared/langs/en-us/starter-files.js
@@ -771,7 +771,7 @@ const EarlyMath = {
       "url": "https://code.pyret.org/editor#share=1wx5GeCHoysnqsc9G9x3Eb5IAYRK5eoMJ&v=0c33ff0"
     },
     "wescheme": {
-      "url": "https://www.wescheme.org/view?publicId=TVUznVqHyA"
+      "url": "https://www.wescheme.org/view?publicId=iEDXtNyn4k"
     }
   },
   "negation-2":{
@@ -780,7 +780,7 @@ const EarlyMath = {
       "url": "https://code.pyret.org/editor#share=1JJcf8ixS0H3xLWYAd-dSbZDk8bs-IQvy&v=0c33ff0"
     },
     "wescheme": {
-      "url": "https://www.wescheme.org/view?publicId=TVUznVqHyA"
+      "url": "https://www.wescheme.org/view?publicId=mu8jTQrApa"
     }
   },
   "distributive-prop": {
