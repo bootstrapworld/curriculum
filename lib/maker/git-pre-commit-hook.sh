ff9465b18015a4d2d45fa533c338388c1ce5b38b
Dorai Sitaram <ds26gte@gmail.com>
2024-04-26 10:29:11 -0400

    git-pre-commit-hook.sh: check added text >= minchars (def. 200) characters, rather than checking num of newlines #2039

--- lib/maker/git-pre-commit-hook.sh
+++ lib/maker/git-pre-commit-hook.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # checks if a committed adoc file has a multi-line proglang.txt alongside it,
-# and if so, checks that newly added text, if more than $minlines lines, does contain @ifproglang.
+# and if so, checks that newly added text, if more than $minchars characters, does contain @ifproglang.
 # If not, issue warning
 
-minlines=10
+minchars=200
 
 for f in $(git diff --name-only --staged); do
   fb=$(basename $f)
@@ -13,7 +13,7 @@ for f in $(git diff --name-only --staged); do
   proglangfile=$d/proglang.txt
   test -f $proglangfile || continue
   test $(grep -c . $proglangfile) -gt 1 || continue
-  test $(git diff --staged -- $f|grep -c '^+') -ge $minlines || continue
+  test $(git diff --staged -- $f|grep '^+'|wc -c) -lt $minchars && continue
   git diff --staged -- $f|grep '^+'|grep -q @ifproglang && continue
   echo WARNING: $f did not have any @ifproglang\'s
 done
