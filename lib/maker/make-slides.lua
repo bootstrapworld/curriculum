9f5845248535fc6c8935018e822e4e3f20be0aba
Dorai Sitaram <ds26gte@gmail.com>
2024-04-28 11:24:29 -0400

    make-slides.lua: advance image count inside @ifnotslide too, to match HTML #2045 (but take care to not count inside |=== inside @ifnotslide)

--- lib/maker/make-slides.lua
+++ lib/maker/make-slides.lua
@@ -203,11 +203,14 @@ local function get_slides(lsn_plan_adoc_file)
           end
         elseif directive == 'ifnotslide' then
           local txt = read_group(i, directive)
-          local _, n = txt:gsub('|===', 'z')
-          n = math.floor(n / 2)
+          local txt2, n = txt:gsub('|===.-|===', 'z')
           tableIdx = tableIdx + n
           _, n = txt:gsub('@show{%(coe', 'z')
           coeIdx = coeIdx + n
+          txt2, n = txt2:gsub('@image{', 'z')
+          imgIdx = imgIdx + n
+          txt2, n = txt2:gsub('@centered-image{', 'z')
+          imgIdx = imgIdx + n
         elseif directive == 'ifproglang' then
           local pls = read_group(i, directive)
           if not pls:match(proglang) then
