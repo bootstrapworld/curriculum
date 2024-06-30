4fcf8d6a19985329c7c2361eeecf192e5849bc4f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2024-06-30 09:59:33 -0400

    update lulu books

--- shared/langs/en-us/course-specifics.rkt
+++ shared/langs/en-us/course-specifics.rkt
@@ -27,9 +27,9 @@
 
 (define *course-workbook-links*
   '(
-    ("algebra-wescheme" "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrap-algebra-wescheme-2024/paperback/product-nvqzezw.html")
-    ("algebra-pyret" "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrapalgebra-pyret-2024/paperback/product-65qn2k4.html")
-    ("data-science" "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrapdata-science-pyret-2024/paperback/product-gjvqqj9.html")
+    ("algebra-wescheme" "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrap-algebra-wescheme-2024/paperback/product-gjgvqy9.html")
+    ("algebra-pyret" "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrapalgebra-pyret-2024/paperback/product-45mn9qg.html")
+    ("data-science" "https://www.lulu.com/shop/nancy-pfenning-and-ben-lerner-and-joe-politz-and-dorai-sitaram/bootstrapdata-science-pyret-2024/paperback/product-dymngdr.html")
     ("data-science-codap" "#")
     ("reactive" "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrap-reactive-pyret-2024/paperback/product-p6k2eme.html")
     ))
