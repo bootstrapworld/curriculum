2ebce7d5f1f9229adda2f2a33fec3bb445930b42
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2024-09-25 21:12:25 -0400

    test revision to learning objectives

---
+++ shared/langs/en-us/learning-objectives.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const jsDir = process.argv[2];
+const finalJsFile = process.argv[3];
+
+if (fs.existsSync(finalJsFile)) {
+  fs.unlinkSync(finalJsFile);
+}
+
+if (fs.existsSync(jsDir)) {
+  // console.log('jsDir', jsDir, 'exists');
+  let ff = fs.readdirSync(jsDir);
+  // console.log('ff len is', ff.length);
+  let allLearningObjectives = {};
+  for (let i = 0; i < ff.length; i++) {
+    let f = jsDir + '/' + ff[i];
+    // console.log('f is', f);
+    if (!f.match(/\.js$/)) { continue; }
+    if (!fs.existsSync(f)) { continue; }
+    // console.log('I');
+    f = f.replace(/\.js$/, '');
+    // console.log('f now is', f);
+    // console.log('II');
+    let j = require(f);
+    // console.log('file', f, 'returns', JSON.stringify(j));
+    allLearningObjectives = { ...allLearningObjectives, ...j };
+  }
+  let jsonString = JSON.stringify(allLearningObjectives);
+  fs.writeFileSync(finalJsFile, 'var allLearningObjectives = ' +
+    jsonString + ';\n');
+}
---
+++ shared/langs/en-us/learning-objectives/data-science.js
@@ -0,0 +1,23 @@
+/*
+ * Example Learning Objective:
+
+  "histogram-skew": {
+    "text": "prose we want to show up in the lesson"
+    "standards": ["std1", "std2"]
+    "assessment-rec": "author notes about how it might be assessed"
+  }
+*/
+
+
+module.exports = {
+  "stdev-understand" : {
+    "text": "Use standard deviation to make judgments about data, and understand the role it plays in those judgements.",
+    "standards": ["HSS.ID.A.2"],
+    "assessment-rec": "Compare two datasets, their means and stdevs, and a conclusion that ignores the stdev. Ask students why the conclusion is wrong"
+  },
+  "stdev-calc" : {
+    "text": "Calculate the standard deviation of a dataset",
+    "standards": ["HSS.ID.A.2"],
+    "assessment-rec": "Give students a dataset, have them calculate the stdev"
+  }
+}
