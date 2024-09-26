50f3202e9eadd24c0bd2375a67b0b2f4f33e12f7
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2024-09-25 21:22:27 -0400

    [DS] more learning objective improvements (#2200)

--- shared/langs/en-us/learning-objectives/data-science.js
+++ shared/langs/en-us/learning-objectives/data-science.js
@@ -19,5 +19,15 @@ module.exports = {
     "text": "Calculate the standard deviation of a dataset",
     "standards": ["HSS.ID.A.2"],
     "assessment-rec": "Give students a dataset, have them calculate the stdev"
+  },
+  "boxplot-create" : {
+    "text": "Compute the 5-number summary and draw the box plot for a given dataset",
+    "standards": ["6.SP.B.4","HSS.ID.A.1"],
+    "assessment-rec": "Give students a dataset, have them calculate the 5-number summary and draw a box plot"
+  },
+  "spread-transfer" : {
+    "text": "compare and contrast information displayed in a box plot and a histogram",
+    "standards": ["6.SP.B.4","HSS.ID.A.1"],
+    "assessment-rec": "Have students match box plots to histograms"
   }
 }
