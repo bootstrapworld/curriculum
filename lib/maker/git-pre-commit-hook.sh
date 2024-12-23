f1fb40092d186033835ec384d33b7099fd70602c
Dorai Sitaram <ds26gte@gmail.com>
2024-12-23 13:40:46 -0500

    commit hook: show related (@ifproglang, spellcheck, bad-json) warnings contiguously

--- lib/maker/git-pre-commit-hook.sh
+++ lib/maker/git-pre-commit-hook.sh
@@ -8,6 +8,14 @@ minchars=200
 
 JQ=$(which jq)
 
+COMMITHOOK_PROGLANG=distribution/.commithook.proglang.log
+COMMITHOOK_SPELLCHECK=distribution/.commithook.spellcheck.log
+COMMITHOOK_BADJSON=distribution/.commithook.badjson.log
+
+rm -f $COMMITHOOK_PROGLANG $COMMITHOOK_SPELLCHECK $COMMITHOOK_BADJSON
+
+touch $COMMITHOOK_PROGLANG $COMMITHOOK_SPELLCHECK $COMMITHOOK_BADJSON
+
 for f in $(git diff --name-only --staged); do
   if test ! -f $f; then continue; fi
   fb=$(basename $f)
@@ -19,19 +27,23 @@ for f in $(git diff --name-only --staged); do
       test $(grep -c . $proglangfile) -gt 1 || continue
       test $(git -c diff.tool=vimdiff diff --staged -- $f|grep '^+'|wc -c) -lt $minchars && continue
       git diff --staged -- $f|grep '^+'|grep -q @ifproglang && continue
-      echo WARNING: $f did not have any @ifproglang\'s
+      echo WARNING: $f did not have any @ifproglang\'s >> $COMMITHOOK_PROGLANG
     done
 
     for doit in justonce; do
-      git diff --staged -- $f|grep '^+'|grep -v '^+++'|node lib/spellcheck.js $f
+      git diff --staged -- $f|grep '^+'|grep -v '^+++'|node lib/spellcheck.js $f >> $COMMITHOOK_SPELLCHECK
     done
 
   elif test ${fb#*.} = json; then
     test -z "$JQ" && continue
     cat $f|$JQ -e >/dev/null 2>&1 && continue
-    echo ERROR: $f is invalid JSON
-    exit 1
+    echo ERROR: $f is invalid JSON >> $COMMITHOOK_BADJSON
+    break
 
   else continue
   fi
 done
+
+cat $COMMITHOOK_PROGLANG
+cat $COMMITHOOK_SPELLCHECK
+cat $COMMITHOOK_BADJSON
