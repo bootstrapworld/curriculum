63ff579326f76ddc4a824c45c107ec5a152a088a
retabak <rachel@bootstrapworld.org>
2025-02-05 15:28:32 -0500

    updates to data-driven-algos

--- shared/langs/en-us/starter-files.js
+++ shared/langs/en-us/starter-files.js
@@ -364,7 +364,7 @@ const AI = {
   "spell-checker": {
     "title": "Spell Checker Starter File",
     "pyret": {
-      "url": "https://code.pyret.org/editor#share=1U8b-lTUhZ9xyrlNsMmRNZwffnwDsbhi3&v=3ddb1dd"
+      "url": "https://code.pyret.org/editor#program=1aOVV_etaxNQKnwL1qUzGoBqhX3F2i2K8"
       }
     },
   "plagiarism": {
