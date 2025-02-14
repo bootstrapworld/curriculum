2ceb15b306c4f9f8a93432ae7cbe510ab0095eac
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2025-02-14 08:07:19 -0500

    consolidate LESS compilation (use technique from the webpack branch) and embed all fonts - PDF printing is getting faster...

--- lib/maker/make-pdf.sh
+++ lib/maker/make-pdf.sh
@@ -6,6 +6,6 @@ if grep -q input $PUPPETEER_INPUT; then
   if test -n "$DEBUGNOBOOK"; then
     echo node lib/html2pdf.js $PUPPETEER_INPUT
   else
-    node lib/html2pdf.js $PUPPETEER_INPUT --tabsPerCore $TPC
+    node lib/html2pdf.js $PUPPETEER_INPUT
   fi
 fi
