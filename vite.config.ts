2fd9cf1dd07eaf01fd74e5ffeac86c0183f0d26f
Emmanuel Schanzer <schanzer@ccs.neu.edu>
2025-03-26 12:44:54 -0400

    first working npx-based build of quiz buried in a path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => ({
-  base: "./lessons/__Authoring",
+  root: "lessons/__Authoring/__sample-lesson/langs/en-us/sample-quiz/",
   plugins: [react()],
-  rollupOptions: {
-      // https://rollupjs.org/configuration-options/
-    },
 }));
