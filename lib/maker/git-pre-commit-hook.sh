3da9b038699b39f2eb39f8f5dd0288cecc865818
Dorai Sitaram <ds26gte@gmail.com>
2025-04-02 01:11:32 -0400

    - git-pre-commit-hook.sh: For .adoc files, determine natlang, and for   now, only spellcheck if it's en-us - skip-non-natlang.lua: For en-us, remove words in the hunspell   exception jsons

--- lib/maker/git-pre-commit-hook.sh
+++ lib/maker/git-pre-commit-hook.sh
@@ -20,9 +20,11 @@ done
 for f in $(git diff --name-only --staged); do
   if test ! -f $f; then continue; fi
   fb=$(basename $f)
+  fd=$(basename $(dirname $f))
   if test ${fb#*.} = adoc; then
     for doit in justonce; do
       d=$(dirname $f)
+      export NATLANG=$(echo $f|sed -e 's/.*\/\(en-us\|es-mx\)\/.*/\1/')
       proglangfile=$d/proglang.txt
       test -f $proglangfile || continue
       test $(grep -c . $proglangfile) -gt 1 || continue
@@ -31,9 +33,11 @@ for f in $(git diff --name-only --staged); do
       echo WARNING: $f did not have any @ifproglang\'s >> $COMMITHOOK_PROGLANG
     done
 
-    for doit in justonce; do
-      git diff --staged -- $f | grep '^+' | grep -v '^+++' | lib/maker/skip-non-natlang.lua | node lib/spellcheck.js $f >> $COMMITHOOK_SPELLCHECK
-    done
+    if test "$NATLANG" = "en-us"; then
+      for doit in justonce; do
+        git diff --staged -- $f | grep '^+' | grep -v '^+++' | lib/maker/skip-non-natlang.lua | node lib/spellcheck.js $f >> $COMMITHOOK_SPELLCHECK
+      done
+    fi
 
   elif test ${fb#*.} = json; then
     test -z "$JQ" && continue
--- lib/maker/skip-non-natlang.lua
+++ lib/maker/skip-non-natlang.lua
@@ -1,10 +1,25 @@
 #! /usr/bin/env lua
 
+local make_dir = 'lib/maker/'
+
+dofile(make_dir .. 'readers.lua')
+dofile(make_dir .. 'jread.lua')
+
 -- reads from the stdin,
 -- removes leading pluses,
 -- removes stuff enclosed in backticks,
 -- converts newlines to spaces,
 -- writes to stdout
+
+local natlang = os.getenv('NATLANG')
+
+local added_words = {}
+local ignored_words = {}
+
+if natlang == 'en-us' then
+  added_words = read_json_file('shared/langs/en-us/hunspell/added-words.json')
+  ignored_words = read_json_file('shared/langs/en-us/hunspell/ignored-words.json')
+end
 
 local function skip_non_natlang(x)
   x = x:gsub('\n%+', ' ')
@@ -12,6 +27,12 @@ local function skip_non_natlang(x)
   x = x:gsub('```.-```', '')
   x = x:gsub('``.-``', '')
   x = x:gsub('`.-`', '')
+  for _,w in ipairs(added_words) do
+    x = x:gsub(w, '')
+  end
+  for _,w in ipairs(ignored_words) do
+    x = x:gsub(w, '')
+  end
   return x
 end
 
