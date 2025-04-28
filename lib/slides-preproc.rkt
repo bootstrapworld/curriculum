ea4b611892d8967503b1333d441aa000079d988b
Dorai Sitaram <ds26gte@gmail.com>
2025-04-28 16:53:40 -0400

    Reapply "[lib] use <span> for inline styles (see #2510)"
    This reverts commit 69b53bd857532e8f1876020c8a841df1ec6524c3.

--- lib/slides-preproc.rkt
+++ lib/slides-preproc.rkt
@@ -733,8 +733,9 @@
                                 (expand-directives:string->port text o)))]
                            [(string=? directive "big")
                             (let ([text (string-trim (read-group i directive #:multiline? #t))])
+                              (display "<span style=\"font-size: 22pt\">" o)
                               (expand-directives:string->port text o)
-                              (display "{style=\"font-size: 22pt\"}" o))]
+                              (display "</span>" o))]
                            [(string=? directive "lesson-point")
                             (let ([text (string-trim (read-group i directive #:multiline? #t))])
                               (display ":pushpin: " o)
