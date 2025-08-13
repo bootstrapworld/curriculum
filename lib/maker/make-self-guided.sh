dd3ea0929dec2d302c95af37a2a4ec07c162f715
Dorai Sitaram <ds26gte@gmail.com>
2025-08-12 21:07:04 -0400

    - build creates self-guided subdirs in distr lessons #2623 - add script make-self-guided.sh

---
+++ lib/maker/make-self-guided.sh
@@ -0,0 +1,42 @@
+#! /usr/bin/env bash
+
+# this is the dir containing the generic parts
+self_guided_dir=$TOPDIR/distribution/$NATLANG/lib/self-guided
+
+function create_local_self_guided() {
+  local g=$1
+  test -f $g/selfGuidedBits.jsx || return
+  (cd $g
+  mkdir -p self-guided
+  cd self-guided
+  for sgf in $(ls $self_guided_dir); do
+    # soft link to node_modules subdir from the generic dir;
+    # other files copied
+    if test -d $self_guided_dir/$sgf; then
+      ln -sf $self_guided_dir/$sgf
+    else
+      cp -p $self_guided_dir/$sgf .
+    fi
+  done
+  ln -sf ../selfGuidedBits.jsx
+  test -d ../images && ln -sf ../images
+  test -d ../videos && ln -sf ../videos
+  )
+}
+
+if test -s $RELEVANT_LESSONS_LIST_FILE; then
+  # if make called with COURSE= only create self-guided dirs for the relevant lessons
+  for f in $(cat $RELEVANT_LESSONS_LIST_FILE); do
+    g=$TOPDIR/distribution/$NATLANG/lessons/$f
+    create_local_self_guided $g
+  done
+else
+  # create self-guided dirs for all lessons
+  for f in $(ls $TOPDIR/distribution/$NATLANG/lessons); do
+    g=$TOPDIR/distribution/$NATLANG/lessons/${f%/}
+    create_local_self_guided $g
+  done
+fi
+
+# keep make happy with explicit exit 0
+exit 0
