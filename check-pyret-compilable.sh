3140c0816bd4c0e79c8052e3ceabc7f560e50285
Dorai Sitaram <ds26gte@gmail.com>
2025-09-23 07:43:24 -0400

    greatly simplify check-pyret-compilable.sh

--- check-pyret-compilable.sh
+++ check-pyret-compilable.sh
@@ -2,25 +2,8 @@
 
 pyret_files_dir=$(pwd)/shared/langs/en-us/pyret-files
 
-function check_pyret_file() {
-  local f=$1
-  local tmpf
-  if test -d "$f"; then
-    tmpf=$(mktemp .check-pyret-compilable-XXXXXX.txt)
-    ls "$f" > $tmpf
-    while read g; do
-      check_pyret_file "$f/$g"
-    done < $tmpf
-    rm $tmpf
-  elif test -f "$f"; then
-    if echo $f | grep -q '\.arr$'; then
-      if pyret -c "$f" 2>&1 | grep -q 'Cleaning up'; then
-        :
-      else
-        echo WARNING: Could not compile $(realpath "$f" --relative-to=$pyret_files_dir)
-      fi
-    fi
+while IFS= read -r pyret_file; do
+  if ! pyret -c "$pyret_file" 2>&1 | grep -q 'Cleaning up'; then
+    echo WARNING: Could not compile $(realpath "$pyret_file" --relative-to=$pyret_files_dir)
   fi
-}
-
-check_pyret_file $pyret_files_dir
+done < <(find $pyret_files_dir -type f -name \*.arr)
