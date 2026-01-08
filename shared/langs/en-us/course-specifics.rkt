8b8fa18e591336790f225b035716b8d2b195db27
Emmanuel Schanzer <schanzer@bootstrapworld.org>
2026-01-08 10:33:45 -0500

    add E+E workbook link

--- shared/langs/en-us/course-specifics.rkt
+++ shared/langs/en-us/course-specifics.rkt
@@ -34,6 +34,7 @@
     ("algebra-2"        "https://www.lulu.com/shop/shriram-krishnamurthi-and-kathi-fisler-and-ben-lerner-and-joe-politz/bootstrap-algebra-2/paperback/product-45mwgrw.html")
     ("reactive"         "https://www.lulu.com/shop/ben-lerner-and-joe-politz-and-dorai-sitaram-and-rachel-tabak/bootstrap-reactive-pyret-2024/paperback/product-p6k2eme.html")
     ("ai"               "https://www.lulu.com/shop/flannery-denny-and-kathi-fisler-and-shriram-krishnamurthi-and-ben-lerner/bootstrapai/paperback/product-zmr88pn.html")
+    ("expressions-and-equations" "https://www.lulu.com/shop/flannery-denny-and-kathi-fisler-and-shriram-krishnamurthi-and-ben-lerner/bootstrapexpressions-and-equations-pyret-2025/paperback/product-gjy5e2d.html")
     ("data-literacy-codap" "#")
     ))
 
