e6fccab2159eb4f58e4ca565338c74ed5033a3ed
Emmanuel Schanzer <schanzer@bootstrapworld.org>
2026-01-20 12:55:50 -0500

    [lib] add integrity checks to images.js and dictionaries.js

--- lib/maker/check-graph.sh
+++ lib/maker/check-graph.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 
-# last modified 2025-07-11
+# last modified 2026-01-20
+
+# check integrity of dependency-graph
 node --trace-uncaught -e 'graph = require("./distribution/en-us/lib/dependency-graph.js");require("./distribution/en-us/lib/bootstraplesson.js").configureGraph(graph);'
