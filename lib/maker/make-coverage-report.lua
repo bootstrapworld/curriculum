ab3d46eb4e289f46c1f068f17486f4f9cc388f0c
Dorai Sitaram <ds26gte@gmail.com>
2026-03-03 11:55:23 -0500

    - make-coverage-report uses .index-objectives.txt.kp to identify lessons that can be associated with a standard via @objective, and highlights them distinctively #2498 - <lesson>/.cached/.index-objectives.txt.kp stores @objective labels - `make clean` scrubs coverage-report.*

--- lib/maker/make-coverage-report.lua
+++ lib/maker/make-coverage-report.lua
@@ -13,6 +13,7 @@ local function print_coverage_script_n_style()
   o:write([[
   <style>
   .unused { background-color: #ffdddd; }
+  .via_objectives_only { background-color: #ddffdd; }
   .coverageElement { display: none; }
   </style>
 
@@ -85,12 +86,12 @@ local function display_selection()
   o:write('++++\n')
 end
 
-local function display_subreport(dictionaries)
+local function display_subreport(dictionaries, standard_to_lessons_map)
   for _,dictionary in ipairs(dictionaries) do
-    -- each dictionary is an array of: nickname, expanded_name, dict_obj, url)
-    -- each dict_obj is a hash mapping label to { description = string, lessons = array }
+    -- each dictionary is an array { nickname, expanded_name, dict_obj, url }
+    -- each dict_obj is a json-hash mapping label to { description = string, lessons = array }
     --
-    -- replace all dots and spaces in label by underscore so it's a valid css class name
+    -- replace all dots & spaces in nickname by underscore so it's a valid css class name
     local nickname = dictionary[1]:gsub('[%.%s]', '_')
     o:write('[.coverageElement.' .. nickname .. ']\n')
     o:write('[cols="2a,1a,7a"]\n')
@@ -100,14 +101,38 @@ local function display_subreport(dictionaries)
       local entry = dict_obj[lbl]
       local desc = entry.description:gsub('|', '&#x7c;')
       local lessons = entry.lessons
-      local count = #lessons
+      local lessons_via_objectives
+      if standard_to_lessons_map then
+        lessons_via_objectives = standard_to_lessons_map[lbl] or {}
+      else
+        lessons_via_objectives = {}
+      end
+      local lessons_via_objectives_only = {}
+      for _,lesson in ipairs(lessons_via_objectives) do
+        if not memberp(lesson, lessons) then
+          table.insert(lessons_via_objectives_only, lesson)
+        end
+      end
+      local count = #lessons + #lessons_via_objectives_only
       if count == 0 then
         o:write('| [.unused]#' .. lbl .. '# ')
         o:write('| [.unused]#none# ')
         o:write('| [.unused]#' .. desc .. '#\n')
       else
         o:write('| ' .. lbl .. ' ')
-        o:write('| ' .. table.concat(lessons, ', ') .. ' (' .. count .. ') ')
+        o:write('| ')
+        if #lessons > 0 then
+          o:write(table.concat(lessons, ', '))
+          if #lessons_via_objectives_only > 0 then
+            o:write(', ')
+          end
+        end
+        if #lessons_via_objectives_only > 0 then
+          o:write('[.via_objectives_only]#')
+          o:write(table.concat(lessons_via_objectives, ', '))
+          o:write('#')
+        end
+        o:write(' (' .. count .. ') ')
         o:write('| ' .. desc .. '\n')
       end
     end
@@ -117,11 +142,57 @@ end
 
 o:write('= Coverage Report\n\n')
 o:write('Each line shows the name of the standard/practice/textbook followed by the number of times it\'s used.\n')
-o:write('If a standard is not used at all, the line is highlighted in red.\n\n')
+o:write('If a standard is not used at all, the line is highlighted in [.unused]#red#.\n')
+o:write('If a lesson is not explicity associated a standard, but is associated with a related objective, it is highlighted in [.via_objectives_only]#green#.\n')
+o:write('\n')
+
+local objectives_list = read_json_file('distribution/en-us/lib/learningObjectives.js')
+
+local standard_to_objectives_map = {}
+
+for objective,hashob in pairs(objectives_list) do
+  local hashob_standards = hashob.standards or {}
+  for _,std in ipairs(hashob_standards) do
+    if not standard_to_objectives_map[std] then
+      standard_to_objectives_map[std] = {}
+    end
+    table.insert(standard_to_objectives_map[std], objective)
+  end
+end
+
+local objective_to_lessons_map = {}
+
+for _,lsn in ipairs(shell_output('ls distribution/en-us/lessons')) do
+  local lesson_objectives_file = 'distribution/en-us/lessons/' .. lsn .. '/.cached/.index-objectives.txt.kp'
+  if file_exists_p(lesson_objectives_file) then
+    local i = io.open(lesson_objectives_file)
+    for obj in i:lines() do
+      if not objective_to_lessons_map[obj] then
+        objective_to_lessons_map[obj] = {}
+      end
+      table.insert(objective_to_lessons_map[obj], lsn)
+    end
+    i:close()
+  end
+end
+
+local standard_to_lessons_map = {}
+
+for std,objs in pairs(standard_to_objectives_map) do
+  if not standard_to_lessons_map[std] then
+    standard_to_lessons_map[std] = {}
+  end
+  for _,obj in ipairs(objs) do
+    local obj_lsns = objective_to_lessons_map[obj] or {}
+    for _,lsn in ipairs(obj_lsns) do
+      table.insert(standard_to_lessons_map[std], lsn)
+    end
+  end
+end
 
 print_coverage_script_n_style()
 display_selection()
-display_subreport(standards_list)
+display_subreport(standards_list, standard_to_lessons_map)
 display_subreport(textbooks_list)
 display_subreport(practices_list)
 
