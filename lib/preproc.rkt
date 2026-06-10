5003c46cca0874e2998a7e6a875c01fbfe08d987
Emmanuel Schanzer <schanzer@bootstrapworld.org>
2026-06-10 16:12:54 -0400

    improvements to blanklines height calculation (#2789)

--- lib/preproc.rkt
+++ lib/preproc.rkt
@@ -1669,7 +1669,10 @@
                                    [height (* n 2.2)] ; each line is 2.2em tall (see shared.less)
                                    [text (read-group i directive #:multiline? #t)])
                               ; (printf "doing @blanklines ~s\n" n)
-                              (display-begin-span ".blanklines" o #:attribs (format "style=\"height: ~aem\"" (* 2.2 n)))
+                              ; height is a placeholder; bootstraplesson.js
+                              ; (drawBlankLines) overrides it to match @fitb
+                              ; line spacing, using data-lines for the count
+                              (display-begin-span ".blanklines" o #:attribs (format "style=\"height: ~aem\" data-lines=\"~a\"" (* 2.2 n) n))
                               (display (expand-directives:string->string text #:enclosing-directive directive) o)
                               (display-end-span o))]
                            [(string=? directive "duration")
